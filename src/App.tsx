e41ab7747cf375282d6d1bd4c1186bd9dcfbd56a
donguk.seo <donguk.seo@modori.io>
2021-06-22 16:40:28 +0900

    install apollo

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,34 @@ import getLibrary from './core/utils/getLibrary';
 import PageProvider from './providers/PageProvider';
 import AppNavigator from './AppNavigator';
 import AssetProvider from './providers/AssetProvider';
+import {
+  ApolloProvider,
+  InMemoryCache,
+  ApolloClient,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:8000/subgraphs/name/elysia-dev/elyfi-subgraph',
+  cache: new InMemoryCache()
+});
 
 const App: React.FC = () => {
   return (
-    <LanguageProvider>
-      <TokenProvider>
-        <Web3ReactProvider getLibrary={getLibrary}>
-          <WalletProvider>
-            <AssetProvider>
-              <PageProvider>
-                <AppNavigator />
-              </PageProvider>
-            </AssetProvider>
-          </WalletProvider>
-        </Web3ReactProvider>
-      </TokenProvider>
-    </LanguageProvider>
+    <ApolloProvider client={client} >
+      <LanguageProvider>
+        <TokenProvider>
+          <Web3ReactProvider getLibrary={getLibrary}>
+            <WalletProvider>
+              <AssetProvider>
+                <PageProvider>
+                  <AppNavigator />
+                </PageProvider>
+              </AssetProvider>
+            </WalletProvider>
+          </Web3ReactProvider>
+        </TokenProvider>
+      </LanguageProvider>
+    </ApolloProvider>
   );
 }
 
