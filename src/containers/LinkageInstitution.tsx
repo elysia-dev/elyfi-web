d5fff90dd11b21ce5d2014208b0c4725be9fa2f0
hyunoh-modori <hyunoh.bae@modori.io>
2021-06-30 17:46:17 +0900

    fix CSP

--- src/containers/LinkageInstitution.tsx
+++ src/containers/LinkageInstitution.tsx
@@ -1,6 +1,6 @@
 import ServiceBackground from 'src/assets/images/service-background.png';
 import 'src/stylesheets/style.scss';
-import Logo from 'src/assets/images/Elysia_Logo.png';
+import Logo from 'src/assets/images/ELYFI.png';
 
 const LinkageInstitution = () => {
 
@@ -18,17 +18,17 @@ const LinkageInstitution = () => {
         </div>
         <div className="linkage__collateral__providers__container">
           <div className="linkage__logo__wrapper">
-            <img src={Logo} alt="Logo" />
+            <img className="linkage__logo" src={Logo} alt="Logo" />
           </div>
           <div className="linkage__collateral__providers">
             <p className="linkage__collateral__providers__title bold">
-              (주) 비스컴퍼니
+              주식회사 엘리파이대부
             </p>
             <p className="linkage__collateral__providers__content">
-              Business License Number: -
+              Business License Number : 115-88-01240
             </p>
             <p className="linkage__collateral__providers__link">
-              Website: https://beescompany.co.kr
+              Website: -
             </p>
           </div>
         </div>
@@ -41,35 +41,35 @@ const LinkageInstitution = () => {
               <p>Collateral Service Provider Deposit</p>
               <p>Deposits by Collateral Service Providers</p>
             </div>
-            <p className="linkage__status__value">$ 1,000.00</p>
+            <p className="linkage__status__value">-</p>
           </div>
           <div className="linkage__status__wrapper">
             <div className="linkage__status">
               <p>Borrow Limit</p>
               <p>Collateral Service Provider Deposit * 10</p>
             </div>
-            <p className="linkage__status__value">$ 1,000.00</p>
+            <p className="linkage__status__value">-</p>
           </div>
           <div className="linkage__status__wrapper">
             <div className="linkage__status">
               <p>Borrowed</p>
               <p>Current Loan Receivable</p>
             </div>
-            <p className="linkage__status__value">$ 1,000.00</p>
+            <p className="linkage__status__value">-</p>
           </div>
           <div className="linkage__status__wrapper">
             <div className="linkage__status">
               <p>Borrow Limit Used</p>
               <p>% of Borrowed relative to Borrow Limit</p>
             </div>
-            <p className="linkage__status__value">100%</p>
+            <p className="linkage__status__value">- %</p>
           </div>
           <div className="linkage__status__wrapper">
             <div className="linkage__status">
               <p>Available Borrow Limit</p>
               <p>Borrow Limit minus Borrowed</p>
             </div>
-            <p className="linkage__status__value">$ 1,000.00</p>
+            <p className="linkage__status__value">-</p>
           </div>
         </div>
         {/* <div className="linkage__service__container">
