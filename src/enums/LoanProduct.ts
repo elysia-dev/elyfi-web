ed2c69a50212394a7d86c4726e94ce9c22799e2c
donguk.seo <donguk.seo@modori.io>
2021-07-20 17:20:31 +0900

    add CommercialRealEstateLoan

--- src/enums/LoanProduct.ts
+++ src/enums/LoanProduct.ts
@@ -1,7 +1,8 @@
 enum LoanProduct {
 	MortgageLoans = 1,
 	AuctionBalanceLoans = 2,
-	ShortTermLoansWithCharterReturns = 3
+	ShortTermLoansWithCharterReturns = 3,
+	CommercialRealEstateLoan = 4
 }
 
 export default LoanProduct
