719b350538743fcd7a4955cd91ccf9cccbe5050a
donguk.seo <donguk.seo@modori.io>
2021-07-21 14:39:53 +0900

    add el price fetcher

--- src/contexts/PriceContext.ts
+++ src/contexts/PriceContext.ts
@@ -2,12 +2,14 @@ import { createContext } from 'react';
 
 export type PriceContextType = {
   elfiPrice: number;
+  elPrice: number;
   loading: boolean;
   error: boolean;
 }
 
 export const initialPriceContext = {
   elfiPrice: 0,
+  elPrice: 0,
   loading: false,
   error: false,
 }
