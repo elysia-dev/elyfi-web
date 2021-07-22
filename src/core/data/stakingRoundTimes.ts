b3358bcda6b8d116ca323b14b58436aa7e80aff3
donguk.seo <donguk.seo@modori.io>
2021-07-22 15:58:04 +0900

    update round times

--- src/core/data/stakingRoundTimes.ts
+++ src/core/data/stakingRoundTimes.ts
@@ -8,27 +8,27 @@ interface IStakingPoolRound {
 const stakingRoundTimes: IStakingPoolRound[] = [
 	{
 		startedAt: moment('2021.7.21 19:00:00 +9:00'),
-		endedAt: moment('2021.8.20 18:59:59 +9:00'),
+		endedAt: moment('2021.8.10 18:59:59 +9:00'),
 	},
 	{
 		startedAt: moment('2021.8.20 19:00:00 +9:00'),
-		endedAt: moment('2021.9.19 18:59:59 +9:00'),
+		endedAt: moment('2021.9.9 18:59:59 +9:00'),
 	},
 	{
 		startedAt: moment('2021.9.19 19:00:00 +9:00'),
-		endedAt: moment('2021.10.19 18:59:59 +9:00'),
+		endedAt: moment('2021.10.9 18:59:59 +9:00'),
 	},
 	{
 		startedAt: moment('2021.10.19 19:00:00 +9:00'),
-		endedAt: moment('2021.11.18 18:59:59 +9:00'),
+		endedAt: moment('2021.11.8 18:59:59 +9:00'),
 	},
 	{
 		startedAt: moment('2021.11.18 19:00:00 +9:00'),
-		endedAt: moment('2021.12.18 18:59:59 +9:00'),
+		endedAt: moment('2021.12.8 18:59:59 +9:00'),
 	},
 	{
 		startedAt: moment('2021.12.18 19:00:00 +9:00'),
-		endedAt: moment('2022.01.17 18:59:59 +9:00'),
+		endedAt: moment('2022.01.7 18:59:59 +9:00'),
 	},
 ]
 
