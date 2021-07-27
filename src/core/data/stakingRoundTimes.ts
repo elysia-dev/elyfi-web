16e72cc4a3af316d11f75dd846e88b6e4505e5ba
hyunoh-modori <hyunoh.bae@modori.io>
2021-07-27 19:26:32 +0900

    change RoundTime

--- src/core/data/stakingRoundTimes.ts
+++ src/core/data/stakingRoundTimes.ts
@@ -7,28 +7,28 @@ interface IStakingPoolRound {
 
 const stakingRoundTimes: IStakingPoolRound[] = [
 	{
-		startedAt: moment('2021.7.21 19:00:00 +9:00'),
-		endedAt: moment('2021.8.10 18:59:59 +9:00'),
+		startedAt: moment('2021.7.27 19:00:00 +9:00'),
+		endedAt: moment('2021.8.16 19:00:00 +9:00'),
 	},
 	{
-		startedAt: moment('2021.8.20 19:00:00 +9:00'),
-		endedAt: moment('2021.9.9 18:59:59 +9:00'),
+		startedAt: moment('2021.8.26 19:00:00 +9:00'),
+		endedAt: moment('2021.9.15 19:00:00 +9:00'),
 	},
 	{
-		startedAt: moment('2021.9.19 19:00:00 +9:00'),
-		endedAt: moment('2021.10.9 18:59:59 +9:00'),
+		startedAt: moment('2021.9.25 19:00:00 +9:00'),
+		endedAt: moment('2021.10.15 19:00:00 +9:00'),
 	},
 	{
-		startedAt: moment('2021.10.19 19:00:00 +9:00'),
-		endedAt: moment('2021.11.8 18:59:59 +9:00'),
+		startedAt: moment('2021.10.25 19:00:00 +9:00'),
+		endedAt: moment('2021.11.14 19:00:00 +9:00'),
 	},
 	{
-		startedAt: moment('2021.11.18 19:00:00 +9:00'),
-		endedAt: moment('2021.12.8 18:59:59 +9:00'),
+		startedAt: moment('2021.11.24 19:00:00 +9:00'),
+		endedAt: moment('2021.12.14 19:00:00 +9:00'),
 	},
 	{
-		startedAt: moment('2021.12.18 19:00:00 +9:00'),
-		endedAt: moment('2022.01.7 18:59:59 +9:00'),
+		startedAt: moment('2021.12.24 19:00:00 +9:00'),
+		endedAt: moment('2022.1.13 19:00:00 +9:00'),
 	},
 ]
 
