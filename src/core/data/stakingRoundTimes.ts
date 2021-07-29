30f822b9d7c3bf62d0d0a632c2689609166b0f33
donguk.seo <donguk.seo@modori.io>
2021-07-29 21:14:04 +0900

    add format to moment

--- src/core/data/stakingRoundTimes.ts
+++ src/core/data/stakingRoundTimes.ts
@@ -5,30 +5,32 @@ interface IStakingPoolRound {
 	endedAt: moment.Moment
 }
 
+const format = 'YYYY.MM.DD hh:mm:ss Z';
+
 const stakingRoundTimes: IStakingPoolRound[] = [
 	{
-		startedAt: moment('2021.7.27 19:00:00 +9:00'),
-		endedAt: moment('2021.8.16 19:00:00 +9:00'),
+		startedAt: moment('2021.7.27 19:00:00 +9:00', format),
+		endedAt: moment('2021.8.16 19:00:00 +9:00', format),
 	},
 	{
-		startedAt: moment('2021.8.26 19:00:00 +9:00'),
-		endedAt: moment('2021.9.15 19:00:00 +9:00'),
+		startedAt: moment('2021.8.26 19:00:00 +9:00', format),
+		endedAt: moment('2021.9.15 19:00:00 +9:00', format),
 	},
 	{
-		startedAt: moment('2021.9.25 19:00:00 +9:00'),
-		endedAt: moment('2021.10.15 19:00:00 +9:00'),
+		startedAt: moment('2021.9.25 19:00:00 +9:00', format),
+		endedAt: moment('2021.10.15 19:00:00 +9:00', format),
 	},
 	{
-		startedAt: moment('2021.10.25 19:00:00 +9:00'),
-		endedAt: moment('2021.11.14 19:00:00 +9:00'),
+		startedAt: moment('2021.10.25 19:00:00 +9:00', format),
+		endedAt: moment('2021.11.14 19:00:00 +9:00', format),
 	},
 	{
-		startedAt: moment('2021.11.24 19:00:00 +9:00'),
-		endedAt: moment('2021.12.14 19:00:00 +9:00'),
+		startedAt: moment('2021.11.24 19:00:00 +9:00', format),
+		endedAt: moment('2021.12.14 19:00:00 +9:00', format),
 	},
 	{
-		startedAt: moment('2021.12.24 19:00:00 +9:00'),
-		endedAt: moment('2022.1.13 19:00:00 +9:00'),
+		startedAt: moment('2021.12.24 19:00:00 +9:00', format),
+		endedAt: moment('2022.1.13 19:00:00 +9:00', format),
 	},
 ]
 
