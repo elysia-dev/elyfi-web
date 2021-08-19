a09f06bb564b3c5939f6203dec652a1e87750678
donguk.seo <donguk.seo@modori.io>
2021-08-19 11:54:41 +0900

    remove unused files

--- src/core/utils/getSignerOrProvider.ts
+++
@@ -1,7 +0,0 @@
-import { providers } from 'ethers';
-
-function getProviderOrSigner(library: providers.Web3Provider, account?: string): providers.Web3Provider | providers.JsonRpcSigner {
-  return account ? library.getSigner(account).connectUnchecked() : library
-}
-
-export default getProviderOrSigner
