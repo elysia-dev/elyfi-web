3242278ca48af6074f250094eb3bf3d7b208cd3a
donguk.seo <donguk.seo@modori.io>
2021-08-26 05:54:53 -0600

    hotfix timezone

--- src/core/data/stakingRoundTimes.ts
+++ src/core/data/stakingRoundTimes.ts
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import 'moment-timezone';
 
 interface IStakingPoolRound {
 	startedAt: moment.Moment
@@ -9,29 +10,34 @@ const format = 'YYYY.MM.DD hh:mm:ss Z';
 
 const stakingRoundTimes: IStakingPoolRound[] = [
 	{
-		startedAt: moment('2021.7.27 19:00:00 +9:00', format),
-		endedAt: moment('2021.8.16 19:00:00 +9:00', format),
+		startedAt: '2021.7.27 19:00:00 +9:00',
+		endedAt: '2021.8.16 19:00:00 +9:00',
 	},
 	{
-		startedAt: moment('2021.8.26 19:00:00 +9:00', format),
-		endedAt: moment('2021.9.15 19:00:00 +9:00', format),
+		startedAt: '2021.8.26 19:00:00 +9:00',
+		endedAt: '2021.9.15 19:00:00 +9:00',
 	},
 	{
-		startedAt: moment('2021.9.25 19:00:00 +9:00', format),
-		endedAt: moment('2021.10.15 19:00:00 +9:00', format),
+		startedAt: '2021.9.25 19:00:00 +9:00',
+		endedAt: '2021.10.15 19:00:00 +9:00',
 	},
 	{
-		startedAt: moment('2021.10.25 19:00:00 +9:00', format),
-		endedAt: moment('2021.11.14 19:00:00 +9:00', format),
+		startedAt: '2021.10.25 19:00:00 +9:00',
+		endedAt: '2021.11.14 19:00:00 +9:00',
 	},
 	{
-		startedAt: moment('2021.11.24 19:00:00 +9:00', format),
-		endedAt: moment('2021.12.14 19:00:00 +9:00', format),
+		startedAt: '2021.11.24 19:00:00 +9:00',
+		endedAt: '2021.12.14 19:00:00 +9:00',
 	},
 	{
-		startedAt: moment('2021.12.24 19:00:00 +9:00', format),
-		endedAt: moment('2022.1.13 19:00:00 +9:00', format),
+		startedAt: '2021.12.24 19:00:00 +9:00',
+		endedAt: '2022.1.13 19:00:00 +9:00',
 	},
-]
+].map((item) => {
+	return {
+		startedAt: moment(item.startedAt, format).tz('Asia/Seoul', true),
+		endedAt: moment(item.endedAt, format).tz('Asia/Seoul', true)
+	}
+})
 
 export default stakingRoundTimes
