5ddd85a1e824a668b58ad61e14d7de16cce295c7
donguk.seo <donguk.seo@modori.io>
2021-11-03 23:39:06 +0900

    add pool price & liquidity

--- src/clients/UniswapV3.ts
+++ src/clients/UniswapV3.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { BigNumber } from 'ethers';
 
 interface IPoolData {
   data: {
@@ -7,6 +8,8 @@ interface IPoolData {
         date: number;
         token1Price: string;
       }[];
+      liquidity: BigNumber;
+      sqrtPrice: BigNumber; // sqrtPriceX96, https://docs.uniswap.org/sdk/guides/fetching-prices#understanding-sqrtprice
     };
   };
 }
@@ -14,21 +17,35 @@ interface IPoolData {
 const baseUrl = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3';
 
 export class UniswapV3 {
-  static getPoolData = async (): Promise<AxiosResponse<IPoolData>> => {
+  static getElfiDaiPoolData = async (): Promise<AxiosResponse<IPoolData>> => {
     return axios.post(baseUrl, {
       query: `
           {
             pool(id: "0xbde484db131bd2ae80e44a57f865c1dfebb7e31f"){
-              token0 {
-                tokenDayData(orderBy: date) {
-                   date,
-                   priceUSD
-                }
-              },
 							poolDayData(orderBy: date) {
 								date,
 								token1Price
-							}
+							},
+              liquidity,
+              sqrtPrice
+            },
+          }
+        `,
+    });
+  };
+
+  static getElfiEthPoolData = async (): Promise<AxiosResponse<IPoolData>> => {
+    // FIXME pool id
+    return axios.post(baseUrl, {
+      query: `
+          {
+            pool(id: "0xbde484db131bd2ae80e44a57f865c1dfebb7e31f"){
+							poolDayData(orderBy: date) {
+								date,
+								token1Price
+							},
+              liquidity,
+              sqrtPrice
             },
           }
         `,
