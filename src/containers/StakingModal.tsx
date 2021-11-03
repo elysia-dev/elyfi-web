2eb54bfa0db23521aec3d670cca40d545a86b12f
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-03 19:48:29 +0900

    Modify prettier

--- src/containers/StakingModal.tsx
+++ src/containers/StakingModal.tsx
@@ -1,12 +1,11 @@
 import { BigNumber, constants, utils } from 'ethers';
-import React, { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react';
 import ELFI from 'src/assets/images/ELFI.png';
 import { formatComma } from 'src/utiles/formatters';
 import envs from 'src/core/envs';
 import { useTranslation } from 'react-i18next';
 import useWatingTx from 'src/hooks/useWaitingTx';
 import LoadingIndicator from 'src/components/LoadingIndicator';
-import { useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import Token from 'src/enums/Token';
 import moment from 'moment';
@@ -20,21 +19,32 @@ import TxContext from 'src/contexts/TxContext';
 import RecentActivityType from 'src/enums/RecentActivityType';
 
 const StakingModal: React.FunctionComponent<{
-  visible: boolean,
-  closeHandler: () => void,
-  afterTx: () => void,
-  stakedToken: Token.ELFI | Token.EL
-  stakedBalance: BigNumber,
-  round: number,
-  endedModal: () => void,
-  setTxStatus: (status: txStatus) => void,
-  setTxWaiting: (status: boolean) => void,
-  transactionModal: () => void
-}> = ({ visible, closeHandler, afterTx, stakedBalance, stakedToken, round, endedModal, setTxStatus, setTxWaiting, transactionModal }) => {
+  visible: boolean;
+  closeHandler: () => void;
+  afterTx: () => void;
+  stakedToken: Token.ELFI | Token.EL;
+  stakedBalance: BigNumber;
+  round: number;
+  endedModal: () => void;
+  setTxStatus: (status: txStatus) => void;
+  setTxWaiting: (status: boolean) => void;
+  transactionModal: () => void;
+}> = ({
+  visible,
+  closeHandler,
+  afterTx,
+  stakedBalance,
+  stakedToken,
+  round,
+  endedModal,
+  setTxStatus,
+  setTxWaiting,
+  transactionModal,
+}) => {
   const { t, i18n } = useTranslation();
   const { account } = useWeb3React();
-  const [stakingMode, setStakingMode] = useState<boolean>(true)
-  const [amount, setAmount] = useState({ value: "", max: false });
+  const [stakingMode, setStakingMode] = useState<boolean>(true);
+  const [amount, setAmount] = useState({ value: '', max: false });
   const current = moment();
   const { setTransaction, failTransaction } = useContext(TxContext);
   const stakingPool = useStakingPool(stakedToken, round >= 3);
@@ -49,20 +59,23 @@ const StakingModal: React.FunctionComponent<{
     stakingPool.address,
   );
   const { waiting, wait } = useWatingTx();
-  const amountLteZero = !amount || utils.parseEther(amount.value || '0').isZero();
+  const amountLteZero =
+    !amount || utils.parseEther(amount.value || '0').isZero();
   const amountGtBalance = utils.parseEther(amount.value || '0').gt(balance);
-  const amountGtStakedBalance = utils.parseEther(amount.value || '0').gt(stakedBalance);
+  const amountGtStakedBalance = utils
+    .parseEther(amount.value || '0')
+    .gt(stakedBalance);
   const initTxTracker = useTxTracking();
 
   useEffect(() => {
     setAmount({
       max: false,
-      value: ''
+      value: '',
     });
-  }, [stakingMode, visible])
+  }, [stakingMode, visible]);
 
   return (
-    <div className="modal" style={{ display: visible ? "block" : "none" }}>
+    <div className="modal" style={{ display: visible ? 'block' : 'none' }}>
       <div className="modal__container">
         <div className="modal__header">
           <div className="modal__header__token-info-wrapper">
@@ -77,18 +90,24 @@ const StakingModal: React.FunctionComponent<{
             </div>
           </div>
         </div>
-        <div className='modal__converter'>
+        <div className="modal__converter">
           <div
-            className={`modal__converter__column${stakingMode ? "--selected" : ""}`}
-            onClick={() => { setStakingMode(true) }}
-          >
-            <p className="bold">{t("staking.staking")}</p>
+            className={`modal__converter__column${
+              stakingMode ? '--selected' : ''
+            }`}
+            onClick={() => {
+              setStakingMode(true);
+            }}>
+            <p className="bold">{t('staking.staking')}</p>
           </div>
           <div
-            className={`modal__converter__column${!stakingMode ? "--selected" : ""}`}
-            onClick={() => { setStakingMode(false) }}
-          >
-            <p className="bold">{t("staking.unstaking")}</p>
+            className={`modal__converter__column${
+              !stakingMode ? '--selected' : ''
+            }`}
+            onClick={() => {
+              setStakingMode(false);
+            }}>
+            <p className="bold">{t('staking.unstaking')}</p>
           </div>
         </div>
         {waiting ? (
@@ -101,14 +120,17 @@ const StakingModal: React.FunctionComponent<{
                   className="modal__maximum bold"
                   onClick={() => {
                     setAmount({
-                      value: (
-                        Math.floor(parseFloat(utils.formatEther(stakingMode ? balance : stakedBalance)))
+                      value: Math.floor(
+                        parseFloat(
+                          utils.formatEther(
+                            stakingMode ? balance : stakedBalance,
+                          ),
+                        ),
                       ).toFixed(8),
-                      max: true
-                    })
-                  }}
-                >
-                  {t("staking.max")}
+                      max: true,
+                    });
+                  }}>
+                  {t('staking.max')}
                 </p>
                 <p className="modal__value bold">
                   <input
@@ -116,164 +138,202 @@ const StakingModal: React.FunctionComponent<{
                     className="modal__text-input"
                     placeholder="0"
                     value={amount.value}
-                    style={{ fontSize: amount.value.length < 8 ? 60 : amount.value.length > 12 ? 35 : 45 }}
+                    style={{
+                      fontSize:
+                        amount.value.length < 8
+                          ? 60
+                          : amount.value.length > 12
+                          ? 35
+                          : 45,
+                    }}
                     onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
-                      ["-", "+", "e"].includes(e.key) && e.preventDefault();
+                      ['-', '+', 'e'].includes(e.key) && e.preventDefault();
                     }}
                     onChange={({ target }) => {
-                      target.value = target.value.replace(/(\.\d{18})\d+/g, '$1');
+                      target.value = target.value.replace(
+                        /(\.\d{18})\d+/g,
+                        '$1',
+                      );
 
                       setAmount({
                         value: target.value,
-                        max: false
-                      })
+                        max: false,
+                      });
                     }}
                   />
                 </p>
               </div>
               <div className="modal__staking__container">
                 <p className="spoqa__bold">
-                  {!stakingMode ? t("staking.available_unstaking_amount") : t("staking.available_staking_amount")}
+                  {!stakingMode
+                    ? t('staking.available_unstaking_amount')
+                    : t('staking.available_staking_amount')}
                 </p>
                 <div>
                   <p className="spoqa__bold">
-                    {
-                      stakingMode ? t("staking.wallet_balance") : t("staking.nth_staking_amount", { nth: toOrdinalNumber(i18n.language, round) })
-                    }
+                    {stakingMode
+                      ? t('staking.wallet_balance')
+                      : t('staking.nth_staking_amount', {
+                          nth: toOrdinalNumber(i18n.language, round),
+                        })}
                   </p>
                   <p className="spoqa__bold">
-                    {`${formatComma(stakingMode ? balance : stakedBalance)} ${stakedToken}`}
+                    {`${formatComma(
+                      stakingMode ? balance : stakedBalance,
+                    )} ${stakedToken}`}
                   </p>
                 </div>
               </div>
-              {
-                !stakingMode ?
-                  <div
-                    className={`modal__button${amountLteZero || amountGtStakedBalance ? "--disable" : ""}`}
-                    onClick={() => {
-                      if (!account || amountLteZero || amountGtStakedBalance) return
+              {!stakingMode ? (
+                <div
+                  className={`modal__button${
+                    amountLteZero || amountGtStakedBalance ? '--disable' : ''
+                  }`}
+                  onClick={() => {
+                    if (!account || amountLteZero || amountGtStakedBalance)
+                      return;
 
-                      const tracker = initTxTracker(
-                        'StakingModal',
-                        'Withdraw',
-                        `${amount.value} ${amount.max} ${stakedToken} ${round}round`
+                    const tracker = initTxTracker(
+                      'StakingModal',
+                      'Withdraw',
+                      `${amount.value} ${amount.max} ${stakedToken} ${round}round`,
+                    );
+                    tracker.clicked();
+
+                    stakingPool
+                      .withdraw(
+                        amount.max
+                          ? constants.MaxUint256
+                          : utils.parseEther(amount.value),
+                        (round >= 3 && stakedToken === Token.ELFI
+                          ? round - 2
+                          : round
+                        ).toString(),
                       )
-                      tracker.clicked();
+                      .then((tx) => {
+                        setTransaction(
+                          tx,
+                          tracker,
+                          (stakedToken +
+                            'StakingWithdraw') as RecentActivityType,
+                          () => {
+                            closeHandler();
+                            transactionModal();
+                          },
+                          () => {
+                            refetch();
+                            afterTx();
+                          },
+                        );
+                      })
+                      .catch((e) => {
+                        failTransaction(tracker, closeHandler, e);
+                      });
+                  }}>
+                  <p>
+                    {amountGtStakedBalance
+                      ? t('staking.insufficient_balance')
+                      : t('staking.unstaking')}
+                  </p>
+                </div>
+              ) : !allowanceLoading && allowance.gte(balance) ? (
+                <div
+                  className={`modal__button${
+                    amountLteZero || amountGtBalance ? '--disable' : ''
+                  }`}
+                  onClick={() => {
+                    if (!account || amountLteZero || amountGtBalance) return;
+                    if (
+                      current.diff(stakingRoundTimes[round - 1].endedAt) > 0
+                    ) {
+                      endedModal();
+                      closeHandler();
+                      return;
+                    }
 
-                      stakingPool
-                        .withdraw(
-                          amount.max ? constants.MaxUint256 : utils.parseEther(amount.value),
-                          ((round >= 3 && stakedToken === Token.ELFI) ? round - 2 : round).toString()
-                        ).then((tx) => {
-                          setTransaction(
-                            tx,
-                            tracker,
-                            stakedToken + "StakingWithdraw" as RecentActivityType,
-                            () => {
-                              closeHandler()
-                              transactionModal()
-                            },
-                            () => {
-                              refetch()
-                              afterTx()
-                            })
-                        }).catch((e) => {
-                          failTransaction(tracker, closeHandler, e);
-                        })
-                    }}
-                  >
-                    <p>
-                      {amountGtStakedBalance ? t("staking.insufficient_balance") : t("staking.unstaking")}
+                    const tracker = initTxTracker(
+                      'StakingModal',
+                      `Stake`,
+                      `${amount.value} ${amount.max} ${stakedToken} ${round}round`,
+                    );
 
-                    </p>
-                  </div> :
-                  !allowanceLoading && allowance.gte(balance) ?
-                    <div
-                      className={`modal__button${amountLteZero || amountGtBalance ? "--disable" : ""}`}
-                      onClick={() => {
-                        if (!account || amountLteZero || amountGtBalance) return
-                        if (current.diff(stakingRoundTimes[round - 1].endedAt) > 0) {
-                          endedModal();
-                          closeHandler();
-                          return;
-                        }
+                    tracker.clicked();
 
-                        const tracker = initTxTracker(
-                          'StakingModal',
-                          `Stake`,
-                          `${amount.value} ${amount.max} ${stakedToken} ${round}round`,
-                        )
+                    // setTxWaiting(true)
 
-                        tracker.clicked();
+                    stakingPool
+                      .stake(
+                        amount.max ? balance : utils.parseEther(amount.value),
+                      )
+                      .then((tx) => {
+                        setTransaction(
+                          tx,
+                          tracker,
+                          (stakedToken + 'Stake') as RecentActivityType,
+                          () => {
+                            closeHandler();
+                            transactionModal();
+                          },
+                          () => {
+                            refetch();
+                            afterTx();
+                          },
+                        );
+                      })
+                      .catch((e) => {
+                        failTransaction(tracker, closeHandler, e);
+                      });
+                  }}>
+                  <p>
+                    {amountGtBalance
+                      ? t('staking.insufficient_balance')
+                      : t('staking.staking')}
+                  </p>
+                </div>
+              ) : (
+                <div
+                  className={'modal__button'}
+                  onClick={() => {
+                    const tracker = initTxTracker(
+                      'StakingModal',
+                      `Approve`,
+                      `${stakedToken} ${round}round`,
+                    );
 
-                        // setTxWaiting(true)
+                    tracker.clicked();
 
-                        stakingPool.stake(amount.max ? balance : utils.parseEther(amount.value)).then((tx) => {
-                          setTransaction(tx,
-                            tracker,
-                            stakedToken + "Stake" as RecentActivityType,
-                            () => {
-                              closeHandler()
-                              transactionModal()
-                            },
-                            () => {
-                              refetch()
-                              afterTx()
-                            }
-                          )
-                        }).catch((e) => {
-                          failTransaction(tracker, closeHandler, e);
-                        })
-                      }}
-                    >
-                      <p>
-                        {amountGtBalance ? t("staking.insufficient_balance") : t("staking.staking")}
-                      </p>
-                    </div> :
-                    <div
-                      className={"modal__button"}
-                      onClick={() => {
-                        const tracker = initTxTracker(
-                          'StakingModal',
-                          `Approve`,
-                          `${stakedToken} ${round}round`,
-                        )
-
-                        tracker.clicked();
-
-                        contract.approve(
-                          stakingPool.address,
-                          constants.MaxUint256,
-                        ).then((tx) => {
-                          setTransaction(
-                            tx,
-                            tracker,
-                            RecentActivityType.Approve,
-                            () => {
-                              closeHandler()
-                              transactionModal()
-                            },
-                            () => {
-                              refetch()
-                              afterTx()
-                            }
-                          )
-                        }).catch((e) => {
-                          failTransaction(tracker, closeHandler, e);
-                        })
-                      }}
-                    >
-                      <p>
-                        {t("dashboard.protocol_allow", { tokenName: stakedToken })}
-                      </p>
-                    </div>
-              }
+                    contract
+                      .approve(stakingPool.address, constants.MaxUint256)
+                      .then((tx) => {
+                        setTransaction(
+                          tx,
+                          tracker,
+                          RecentActivityType.Approve,
+                          () => {
+                            closeHandler();
+                            transactionModal();
+                          },
+                          () => {
+                            refetch();
+                            afterTx();
+                          },
+                        );
+                      })
+                      .catch((e) => {
+                        failTransaction(tracker, closeHandler, e);
+                      });
+                  }}>
+                  <p>
+                    {t('dashboard.protocol_allow', { tokenName: stakedToken })}
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         )}
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 export default StakingModal;
