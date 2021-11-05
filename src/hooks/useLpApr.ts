8609e8280d8b2d3116b1b4c264ebdbde6c0a0558
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-05 16:10:22 +0900

    Modify prettier

--- src/hooks/useLpApr.ts
+++ src/hooks/useLpApr.ts
@@ -11,31 +11,37 @@ const DAYS = 40;
 
 // 풀의 유동성 : liquidity * sqrtPrice ^2
 // Input: stakedToken(-> rewardToken)
-const useLpApr = (stakedToken: Token.ELFI_DAI_LP | Token.ELFI_ETH_LP, stakedLiquidity: BigNumber): string => {
-  const {
-    elfiDaiPool,
-    elfiEthPool,
-    ethPrice,
-    elfiPrice,
-  } = useContext(PriceContext);
+const useLpApr = (
+  stakedToken: Token.ELFI_DAI_LP | Token.ELFI_ETH_LP,
+  stakedLiquidity: BigNumber,
+): string => {
+  const { elfiDaiPool, elfiEthPool, ethPrice, elfiPrice } =
+    useContext(PriceContext);
 
-  const apy = useMemo(() => {
+  const apr = useMemo(() => {
     if (stakedLiquidity.isZero()) return '-';
 
     if (stakedToken === Token.ELFI_DAI_LP) {
       return toCompact(
-        ((ELFI_AMOUNT_PER_POOL * elfiPrice + DAI_AMOUNT_PER_POOL) * 365 / DAYS)
-        / (parseFloat(utils.formatEther(stakedLiquidity)) * elfiDaiPool.price)
-      )
+        (((ELFI_AMOUNT_PER_POOL * elfiPrice + DAI_AMOUNT_PER_POOL) * 365) /
+          DAYS /
+          (parseFloat(utils.formatEther(stakedLiquidity)) *
+            elfiDaiPool.price)) *
+          100,
+      );
     } else {
       return toCompact(
-        ((ELFI_AMOUNT_PER_POOL * elfiPrice + ETH_AMOUNT_PER_POOL * ethPrice) * 365 / DAYS)
-        / (parseFloat(utils.formatEther(stakedLiquidity)) * elfiEthPool.price)
-      )
+        (((ELFI_AMOUNT_PER_POOL * elfiPrice + ETH_AMOUNT_PER_POOL * ethPrice) *
+          365) /
+          DAYS /
+          (parseFloat(utils.formatEther(stakedLiquidity)) *
+            elfiEthPool.price)) *
+          100,
+      );
     }
-  }, [])
+  }, []);
 
-  return apy;
+  return apr;
 };
 
 export default useLpApr;
