61a030782676f2d605fa5e8fa98d799bede4267b
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-05 16:17:51 +0900

    Modify i18n

--- src/components/LpStaking/LpReceiveToken.tsx
+++ src/components/LpStaking/LpReceiveToken.tsx
@@ -1,21 +1,26 @@
+import { Trans, useTranslation } from 'react-i18next';
 import envs from 'src/core/envs';
 import Guide from '../Guide';
 import LpButton from './LpButton';
 
 type Props = {
-  stakedToken: string;
   firstToken: string;
   secondToken: string;
 };
 
 function LpReceiveToken(props: Props) {
   const { firstToken, secondToken } = props;
+  const { t } = useTranslation();
+
   return (
     <div className="lp_receive_token_wrapper">
       <div>
         <div className="spoqa__bold">
-          {`${firstToken}-${secondToken} LP 토큰`}
-          <Guide />
+          {t('lpstaking.lp_token_staking_modal', {
+            firstToken,
+            secondToken,
+          })}
+          <Guide content={t('guide.create_liquidity')} />
         </div>
         <div>
           <a
@@ -27,14 +32,18 @@ function LpReceiveToken(props: Props) {
                 ? `https://app.uniswap.org/#/add/${envs.governanceAddress}/${envs.wEth}`
                 : `https://app.uniswap.org/#/add/${envs.governanceAddress}/${envs.daiAddress}`
             }>
-            <button>LP Token 받기</button>
+            <button>{t('lpstaking.receive_lp_token')}</button>
           </a>
         </div>
       </div>
-      <div className="spoqa">
-        {`Uniswap V3 에 있는 ${firstToken}-${secondToken} 풀에 유동성을 제공함으로써`}{' '}
-        <span className="spoqa__bold">{`${firstToken}-${secondToken} LP`}</span>{' '}
-        토큰을 받으실 수 있습니다!
+      <div className="spoqa i18n">
+        <Trans
+          i18nKey="lpstaking.receive_lp_token_content"
+          values={{
+            firstToken,
+            secondToken,
+          }}
+        />
       </div>
     </div>
   );
