e08c88182d75a94328f257e5059a01b75ce93137
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-05 16:18:08 +0900

    Modify lp stakingbutton

--- src/components/LpStaking/LpButton.tsx
+++ src/components/LpStaking/LpButton.tsx
@@ -1,3 +1,6 @@
+import moment from 'moment';
+import lpStakingTime from 'src/core/data/lpStakingTime';
+
 type Props = {
   onHandler: () => void;
   btnTitle: string;
@@ -5,8 +8,16 @@ type Props = {
 
 function LpButton(props: Props) {
   const { onHandler, btnTitle } = props;
+  const isStakingDate = moment().isBefore(lpStakingTime.startedAt);
+
   return (
-    <button className="spoqa__medium lp_button" onClick={() => onHandler()}>
+    <button
+      className="spoqa__medium lp_button"
+      style={{
+        background: isStakingDate ? '#f8f8f8' : undefined,
+        color: isStakingDate ? '#949494' : undefined,
+      }}
+      onClick={() => (isStakingDate ? '' : onHandler())}>
       {btnTitle}
     </button>
   );
