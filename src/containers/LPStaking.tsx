d1ab020c02e963f652bf7401823ce7ce06b760ba
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-05 18:52:47 +0900

    Modify useeffect

--- src/containers/LPStaking.tsx
+++ src/containers/LPStaking.tsx
@@ -40,13 +40,13 @@ function LPStaking() {
     StakerSubgraph.getPositionsByOwner(account!).then((res) => {
       setPositions(res.data.data.positions);
     });
-  }, [positions, LpTokenPoolSubgraph.getPositionsByOwner]);
+  }, [positions]);
 
   const getPoolPositions = useCallback(() => {
     LpTokenPoolSubgraph.getPositionsByOwner(account!).then((res) => {
       setLpTokens(res.data.data.positions);
     });
-  }, [lpTokens, LpTokenPoolSubgraph.getPositionsByOwner]);
+  }, [lpTokens]);
 
   const getTotalLiquidity = useCallback(() => {
     setIsLoading(true);
@@ -65,7 +65,6 @@ function LPStaking() {
           (sum, current) => sum.add(current.position.liquidity),
           constants.Zero,
         );
-
       setTotalLiquidity({
         ...totalLiquidity,
         daiElfiPoolTotalLiquidity: parseFloat(
@@ -190,7 +189,10 @@ function LPStaking() {
             />
           </div>
         </div>
-        <StakedLp positions={positions.filter((position) => position.staked)} />
+        <StakedLp
+          positions={positions.filter((position) => position.staked)}
+          setPositions={setPositions}
+        />
         <LpReward />
       </section>
     </>
