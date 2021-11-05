4f19742281adfbb72ec1a8d15b8d62cae14dd46a
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-05 16:11:49 +0900

    Add lp staking time

--- src/core/data/lpStakingTime.ts
+++ src/core/data/lpStakingTime.ts
@@ -9,7 +9,7 @@ interface IStakingPool {
 const format = 'YYYY.MM.DD hh:mm:ss Z';
 
 const lpStakingTime: IStakingPool = {
-  startedAt: moment('2021.11.01 19:00:00 +9:00', format).tz('Asia/Seoul', true),
+  startedAt: moment('2021.11.05 19:00:00 +9:00', format).tz('Asia/Seoul', true),
   endedAt: moment('2021.12.15 19:00:00 +9:00', format).tz('Asia/Seoul', true),
 };
 
