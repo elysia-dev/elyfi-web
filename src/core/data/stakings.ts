ee85dbb103a12a66065eaedf99986eed1a860b98
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-16 18:57:58 +0900

    Add mintedPerday

--- src/core/data/stakings.ts
+++ src/core/data/stakings.ts
@@ -3,3 +3,9 @@ import { utils } from 'ethers';
 export const ELFIPerDayOnELStakingPool = utils.parseEther('25000');
 
 export const DAIPerDayOnELFIStakingPool = utils.parseEther('1250');
+
+export const ELFIPerDayOnLpStakingPool = 7500;
+
+export const DAIPerDayOnElfiDaiPool = 625;
+
+export const ETHPerDayOnElfiEthPool = 0.1376;
