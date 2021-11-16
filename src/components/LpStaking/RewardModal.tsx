00f280c026dc1a44ab066a38df4802c9d37fd624
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-16 18:51:10 +0900

    Modify Props & hooks

--- src/components/LpStaking/RewardModal.tsx
+++ src/components/LpStaking/RewardModal.tsx
@@ -1,82 +1,32 @@
-import {
-  BigNumber,
-  constants,
-  ContractTransaction,
-  ethers,
-  providers,
-  utils,
-} from 'ethers';
-import { useContext, useState, useEffect } from 'react';
-import envs from 'src/core/envs';
+import { useContext, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import useWatingTx from 'src/hooks/useWaitingTx';
-import { useWeb3React } from '@web3-react/core';
 import Token from 'src/enums/Token';
-import moment from 'moment';
-import useStakingPool from 'src/hooks/useStakingPool';
-import useERC20Info from 'src/hooks/useERC20Info';
-import useTxTracking from 'src/hooks/useTxTracking';
-import txStatus from 'src/enums/TxStatus';
 import TxContext from 'src/contexts/TxContext';
 import elfi from 'src/assets/images/ELFI.png';
 import eth from 'src/assets/images/eth-color.png';
 import dai from 'src/assets/images/dai.png';
-import stakerABI from 'src/core/abi/StakerABI.json';
 import { formatSixFracionDigit } from 'src/utiles/formatters';
-import RecentActivityType from 'src/enums/RecentActivityType';
+import useClaimReward from 'src/hooks/useClaimReward';
+import RewardTypes from 'src/core/types/RewardTypes';
 
 const RewardModal: React.FunctionComponent<{
   visible: boolean;
   closeHandler: () => void;
-  totalReward: {
-    elfiReward: number;
-    wEthReward: number;
-    daiReward: number;
-  };
-}> = ({ visible, closeHandler, totalReward }) => {
+  rewardToReceive: RewardTypes;
+}> = ({ visible, closeHandler, rewardToReceive }) => {
   const { t } = useTranslation();
-  const { account, library } = useWeb3React();
-  const { setTransaction } = useContext(TxContext);
-  const initTxTracker = useTxTracking();
+  const { txWaiting } = useContext(TxContext);
+  const claim = useClaimReward();
 
-  const receiveRewardHandler = async () => {
-    const staker = new ethers.Contract(
-      envs.stakerAddress,
-      stakerABI,
-      library.getSigner(),
-    );
-    const iFace = new ethers.utils.Interface(stakerABI);
-    const callOne = iFace.encodeFunctionData('claimReward', [
-      envs.governanceAddress,
-      account,
-      0,
-    ]);
-    const callTwo = iFace.encodeFunctionData('claimReward', [
-      envs.daiAddress,
-      account,
-      0,
-    ]);
-    const callThree = iFace.encodeFunctionData('claimReward', [
-      envs.wEthAddress,
-      account,
-      0,
-    ]);
-    const res: ContractTransaction = await staker.multicall([
-      callOne,
-      callTwo,
-      callThree,
-    ]);
-    const tracker = initTxTracker('LpStakingModal', 'ClaimReward', ``);
-    setTransaction(
-      res,
-      tracker,
-      'Claim' as RecentActivityType,
-      () => {
-        closeHandler();
-      },
-      () => {},
-    );
+  const receiveRewardHandler = () => {
+    claim();
   };
+
+  useEffect(() => {
+    if (txWaiting) {
+      closeHandler();
+    }
+  }, [txWaiting]);
 
   return (
     <div className="modal" style={{ display: visible ? 'block' : 'none' }}>
@@ -106,7 +56,7 @@ const RewardModal: React.FunctionComponent<{
               {Token.ELFI}
             </div>
             <div className="lp_reward_elfi">
-              {formatSixFracionDigit(totalReward.elfiReward)}
+              {formatSixFracionDigit(rewardToReceive.elfiReward)}
               <div>{Token.ELFI}</div>
             </div>
           </div>
@@ -116,7 +66,7 @@ const RewardModal: React.FunctionComponent<{
               {Token.ETH}
             </div>
             <div className="lp_reward_eth">
-              {formatSixFracionDigit(totalReward.wEthReward)}
+              {formatSixFracionDigit(rewardToReceive.ethReward)}
               <div>{Token.ETH}</div>
             </div>
           </div>
@@ -130,7 +80,7 @@ const RewardModal: React.FunctionComponent<{
               {Token.DAI}
             </div>
             <div className="lp_reward_dai">
-              {formatSixFracionDigit(totalReward.daiReward)}
+              {formatSixFracionDigit(rewardToReceive.daiReward)}
               <div>{Token.DAI}</div>
             </div>
           </div>
