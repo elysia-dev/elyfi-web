a261c4743f340e88bee85bac2d38a0c7ee74b6e3
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-16 19:02:30 +0900

    Add withdraw hooks

---
+++ src/hooks/useLpWithdraw.ts
@@ -0,0 +1,65 @@
+import { useWeb3React } from '@web3-react/core';
+import { ContractTransaction, ethers } from 'ethers';
+import envs from 'src/core/envs';
+import stakerABI from 'src/core/abi/StakerABI.json';
+import { useContext } from 'react';
+import TxContext from 'src/contexts/TxContext';
+import RecentActivityType from 'src/enums/RecentActivityType';
+import { lpTokenValues } from 'src/utiles/lpTokenValues';
+import useTxTracking from './useTxTracking';
+
+const useLpWithdraw: () => (
+  poolAddress: string,
+  rewardTokenAddress: string,
+  tokenId: string,
+) => void = () => {
+  const { account, library } = useWeb3React();
+  const { setTransaction } = useContext(TxContext);
+  const initTxTracker = useTxTracking();
+  const staker = new ethers.Contract(
+    envs.stakerAddress,
+    stakerABI,
+    library.getSigner(),
+  );
+  const iFace = new ethers.utils.Interface(stakerABI);
+
+  const unstake = async (
+    poolAddress: string,
+    rewardTokenAddress: string,
+    tokenId: string,
+  ) => {
+    try {
+      const elfiUnstake = iFace.encodeFunctionData('unstakeToken', [
+        lpTokenValues(poolAddress, envs.governanceAddress),
+        tokenId,
+      ]);
+      const rewardUnstake = iFace.encodeFunctionData('unstakeToken', [
+        lpTokenValues(poolAddress, rewardTokenAddress),
+        tokenId,
+      ]);
+      const withdraw = iFace.encodeFunctionData('withdrawToken', [
+        tokenId,
+        account,
+        '0x',
+      ]);
+      const res = await staker.multicall([
+        elfiUnstake,
+        rewardUnstake,
+        withdraw,
+      ]);
+      const tracker = initTxTracker('LpUnstaking', 'unstaking', ``);
+      setTransaction(
+        res,
+        tracker,
+        'Withdraw' as RecentActivityType,
+        () => {},
+        () => {},
+      );
+    } catch (error) {
+      throw Error(error);
+    }
+  };
+  return unstake;
+};
+
+export default useLpWithdraw;
