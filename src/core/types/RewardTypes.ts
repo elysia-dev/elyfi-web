35bcf7d5fdf3ce2e95d3fb4eb7105f57901389e1
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-17 15:32:42 +0900

    Modify totalexpectedReward type

--- src/core/types/RewardTypes.ts
+++ src/core/types/RewardTypes.ts
@@ -1,3 +1,13 @@
+export type ExpectedRewardTypes = {
+  beforeElfiReward: number;
+  elfiReward: number;
+  beforeEthReward: number;
+  ethReward: number;
+  beforeDaiReward: number;
+  daiReward: number;
+  tokenId: number;
+};
+
 type RewardTypes = {
   elfiReward: number;
   ethReward: number;
