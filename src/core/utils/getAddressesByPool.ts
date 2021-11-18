b4be76cc634522d0ee1b36ea38a56638cdc0a840
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-18 15:44:50 +0900

    Modify return type

--- src/core/utils/getAddressesByPool.ts
+++ src/core/utils/getAddressesByPool.ts
@@ -1,14 +1,19 @@
 import envs from 'src/core/envs';
 import Position from '../types/Position';
 
-export const isEthElfiPoolAddress = (position: Position) => {
+export const isEthElfiPoolAddress = (position: Position): boolean => {
   return (
     position.incentivePotisions[0].incentive.pool.toLowerCase() ===
     envs.ethElfiPoolAddress.toLowerCase()
   );
 };
 
-const getAddressesByPool = (position: Position) => {
+const getAddressesByPool = (
+  position: Position,
+): {
+  poolAddress: string;
+  rewardTokenAddress: string;
+} => {
   const poolAddress = isEthElfiPoolAddress(position)
     ? envs.ethElfiPoolAddress
     : envs.daiElfiPoolAddress;
