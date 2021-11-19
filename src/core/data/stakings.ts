27ee2b0178d156bd11f99624da0d1c675136d27e
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-19 09:56:14 +0900

    Add total Reward token

--- src/core/data/stakings.ts
+++ src/core/data/stakings.ts
@@ -4,8 +4,12 @@ export const ELFIPerDayOnELStakingPool = utils.parseEther('25000');
 
 export const DAIPerDayOnELFIStakingPool = utils.parseEther('1250');
 
+export const TETHERPerDayOnELFIStakingPool = utils.parseEther('1250');
+
 export const ELFIPerDayOnLpStakingPool = 7500;
-
 export const DAIPerDayOnElfiDaiPool = 625;
-
 export const ETHPerDayOnElfiEthPool = 0.1376;
+
+export const ELFI_REWARD_PER_POOL = 300000;
+export const ETH_REWARD_PER_POOL = 5.507;
+export const DAI_REWARD_PER_POOL = 25000;
