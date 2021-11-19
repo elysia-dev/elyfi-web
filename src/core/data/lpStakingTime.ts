41af6d1ad6eb734c834363b1ebf3ed0fb76fdcf4
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-19 09:57:19 +0900

    Add lpstaking date

--- src/core/data/lpStakingTime.ts
+++ src/core/data/lpStakingTime.ts
@@ -14,3 +14,13 @@ const lpStakingTime: IStakingPool = {
 };
 
 export default lpStakingTime;
+
+export const lpStakingStartedAt = moment(
+  '2021.11.05 19:00:00 +9:00',
+  'YYYY.MM.DD hh:mm:ss Z',
+);
+
+export const lpStakingEndedAt = moment(
+  '2021.12.15 19:00:00 +9:00',
+  'YYYY.MM.DD hh:mm:ss Z',
+);
