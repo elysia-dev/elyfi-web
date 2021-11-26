fcb5b0f1e2fbcadba8bdaef8988e8e43e3959507
Jung gyu cheol <kcpooh0202@naver.com>
2021-11-26 18:46:07 +0900

    Add img

--- src/components/Texts.tsx
+++ src/components/Texts.tsx
@@ -1,11 +1,25 @@
 import { CSSProperties, FunctionComponent } from 'react';
+import elfi from 'src/assets/images/ELFI.png';
+import el from 'src/assets/images/el.png';
+import Token from 'src/enums/Token';
 
 export const Title: FunctionComponent<{
+  stakedToken: Token.EL | Token.ELFI;
   label: string;
   style?: CSSProperties;
-}> = ({ label, style }) => {
+}> = ({ label, style, stakedToken }) => {
+  const tokenImg = stakedToken === Token.EL ? el : elfi;
   return (
     <div className="text__title" style={style}>
+      <img
+        src={tokenImg}
+        alt={stakedToken}
+        style={{
+          marginRight: 10,
+          width: 40,
+          height: 40,
+        }}
+      />
       <p className="bold">{label}</p>
       <hr />
     </div>
