03fb17b7d94f9a1024763a939ab1045a7025b670
Jung gyu cheol <kcpooh0202@naver.com>
2021-12-29 22:08:03 +0900

    Modify 2round deposit reward

--- src/components/RewardPlan/RewardDetailInfo.tsx
+++ src/components/RewardPlan/RewardDetailInfo.tsx
@@ -2,6 +2,7 @@ import { BigNumber } from 'ethers';
 import { FunctionComponent, ReactElement } from 'react';
 import CountUp from 'react-countup';
 import { useTranslation } from 'react-i18next';
+import Token from 'src/enums/Token';
 import { formatCommaSmallFourDisits } from 'src/utiles/formatters';
 
 type Props = {
@@ -11,21 +12,61 @@ type Props = {
   miningEnd: number;
   miningDescription: string[][];
   unit: string;
+  depositRound?: {
+    daiRound: number;
+    tetherRound: number;
+  };
+  token?: Token.DAI | Token.USDT;
+  index?: number;
 };
 
 const RewardDetailInfo: FunctionComponent<Props> = (props) => {
   const { t } = useTranslation();
 
+  const remaining = () => (
+    <div>
+      <p>
+        {props.unit === 'ELFI'
+          ? t('reward.mining_limit')
+          : t('reward.reward_limit')}
+      </p>
+      <p>
+        <CountUp
+          start={props.miningStart}
+          end={props.miningEnd}
+          decimals={4}
+          duration={1}
+          formattingFn={(number) => formatCommaSmallFourDisits(number)}
+        />
+        {` ${props.unit}`}
+      </p>
+    </div>
+  );
+
+  const miningDescription = () =>
+    props.miningDescription.map((data, _index) => {
+      return (
+        <div>
+          <p>{data[0]}</p>
+          <p className="data">{data[1]}</p>
+        </div>
+      );
+    });
+
   return (
     <div className="component__data-info">
-      {props.miningDescription.map((data, _index) => {
-        return (
+      {props.depositRound ? (
+        props.index === 0 ? (
+          miningDescription()
+        ) : (
           <div>
-            <p>{data[0]}</p>
-            <p className="data">{data[1]}</p>
+            <p>{t('reward.daily_mining')}</p>
+            <p className="data">16,666.6667 ELFI</p>
           </div>
-        );
-      })}
+        )
+      ) : (
+        miningDescription()
+      )}
       <div>
         <p>
           {props.unit === 'ELFI'
@@ -43,23 +84,7 @@ const RewardDetailInfo: FunctionComponent<Props> = (props) => {
           {` ${props.unit}`}
         </p>
       </div>
-      <div>
-        <p>
-          {props.unit === 'ELFI'
-            ? t('reward.mining_limit')
-            : t('reward.reward_limit')}
-        </p>
-        <p>
-          <CountUp
-            start={props.miningStart}
-            end={props.miningEnd}
-            decimals={4}
-            duration={1}
-            formattingFn={(number) => formatCommaSmallFourDisits(number)}
-          />
-          {` ${props.unit}`}
-        </p>
-      </div>
+      {props.depositRound ? props.index === 0 && remaining() : remaining()}
     </div>
   );
 };
