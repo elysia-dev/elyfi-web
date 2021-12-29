85f163d7461b8a1979d77e1aadb5d0e471f13abe
Jung gyu cheol <kcpooh0202@naver.com>
2021-12-29 22:09:05 +0900

    Add deposit time

--- src/core/data/moneypoolTimes.ts
+++ src/core/data/moneypoolTimes.ts
@@ -16,3 +16,25 @@ export const tetherMoneyPoolEndedAt = moment(
   '2022.01.11 19:00:00 +9:00',
   'YYYY.MM.DD hh:mm:ss Z',
 );
+
+export const daiMoneyPoolTime = [
+  {
+    startedAt: moment('2021.07.15 10:42:33 +0', 'YYYY.MM.DD hh:mm:ss Z'),
+    endedAt: moneyPoolEndedAt,
+  },
+  {
+    startedAt: moment('2022.01.11 19:00:00 +9:00', 'YYYY.MM.DD hh:mm:ss Z'),
+    endedAt: '',
+  },
+];
+
+export const tetherMoneyPoolTime = [
+  {
+    startedAt: tetherMoneyPoolStartedAt,
+    endedAt: tetherMoneyPoolEndedAt,
+  },
+  {
+    startedAt: moment('2022.01.11 19:00:00 +9:00', 'YYYY.MM.DD hh:mm:ss Z'),
+    endedAt: '',
+  },
+];
