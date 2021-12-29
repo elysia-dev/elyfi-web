89f6dd22a02cd83f3084cbfc65873254ec2a64c7
Jung gyu cheol <kcpooh0202@naver.com>
2021-12-29 13:44:40 +0900

    Modify '/'

--- src/clients/OffChainTopic.ts
+++ src/clients/OffChainTopic.ts
@@ -32,9 +32,9 @@ const baseURL =
 
 export default class OffChainTopic {
   static getTopicList = async (): Promise<AxiosResponse<TopicList>> => {
-    return axios.get(`${baseURL}/c/nap/10.json`);
+    return axios.get(`${baseURL}c/nap/10.json`);
   };
   static getTopicResult = async (topicID: number): Promise<AxiosResponse> => {
-    return axios.get(`${baseURL}/t/${topicID}.json`);
+    return axios.get(`${baseURL}t/${topicID}.json`);
   };
 }
