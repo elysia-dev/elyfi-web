5e8dccd8e7bc7ff0ecc9187cd2428104beb80584
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-05 14:07:40 +0900

    Modify moneypoolAddress

--- src/hooks/useMoneyPool.ts
+++ src/hooks/useMoneyPool.ts
@@ -1,14 +1,16 @@
 import { useWeb3React } from '@web3-react/core';
-import { useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 import { MoneyPool__factory } from '@elysia-dev/contract-typechain';
 import envs from 'src/core/envs';
+import ReservesContext from 'src/contexts/ReservesContext';
 
 const useMoneyPool = () => {
   const { library } = useWeb3React();
+  const { round } = useContext(ReservesContext);
   const contract = useMemo(() => {
     if (!library) return;
     return MoneyPool__factory.connect(
-      envs.moneyPoolAddress,
+      round === 1 ? envs.moneyPoolAddress1st : envs.moneyPoolAddress1st,
       library.getSigner(),
     );
   }, [library]);
