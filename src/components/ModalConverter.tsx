824486d5768a00a28c039aa6598d4f87dd525519
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-05 16:06:12 +0900

    Add isEndedRound

--- src/components/ModalConverter.tsx
+++ src/components/ModalConverter.tsx
@@ -2,16 +2,21 @@ const ModalConverter: React.FunctionComponent<{
   handlerProps: boolean;
   title: string[];
   setState: (value: React.SetStateAction<boolean>) => void;
-}> = ({ handlerProps, title, setState }) => {
+  isEndedRound: boolean;
+}> = ({ handlerProps, title, setState, isEndedRound }) => {
   return (
     <div className="modal__converter">
-      <div
-        className={`modal__converter__column${handlerProps ? ' selected' : ''}`}
-        onClick={() => {
-          setState(true);
-        }}>
-        <h2>{title[0]}</h2>
-      </div>
+      {isEndedRound && (
+        <div
+          className={`modal__converter__column${
+            handlerProps ? ' selected' : ''
+          }`}
+          onClick={() => {
+            setState(true);
+          }}>
+          <h2>{title[0]}</h2>
+        </div>
+      )}
       {title.length === 2 && (
         <div
           className={`modal__converter__column${
