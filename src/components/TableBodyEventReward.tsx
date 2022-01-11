9a9c2fb2fb1be407cd559050bb1d8cc909ee77c2
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-11 17:36:08 +0900

    Add additional reward component

--- src/components/TableBodyEventReward.tsx
+++ src/components/TableBodyEventReward.tsx
@@ -2,62 +2,118 @@ import { useWeb3React } from '@web3-react/core';
 import { BigNumber } from 'ethers';
 import { formatEther } from 'ethers/lib/utils';
 import moment from 'moment';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useContext } from 'react';
 import CountUp from 'react-countup';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import ReservesContext from 'src/contexts/ReservesContext';
+import MediaQuery from 'src/enums/MediaQuery';
+import Token from 'src/enums/Token';
+import useMediaQueryType from 'src/hooks/useMediaQueryType';
 import { formatSixFracionDigit } from 'src/utiles/formatters';
 
 type Props = {
   moneyPoolTime: string;
-  expectedAddIncentiveBefore: BigNumber;
-  expectedAddIncentiveAfter: BigNumber;
+  expectedAdditionalIncentiveBefore: BigNumber;
+  expectedAdditionalIncentiveAfter: BigNumber;
+  buttonEvent: ((e: any) => void) | undefined;
+  tokenName: Token.DAI | Token.USDT;
 };
 
 const TableBodyEventReward: FunctionComponent<Props> = ({
   moneyPoolTime,
-  expectedAddIncentiveAfter,
-  expectedAddIncentiveBefore,
+  expectedAdditionalIncentiveAfter,
+  expectedAdditionalIncentiveBefore,
+  buttonEvent,
+  tokenName,
 }) => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const { account } = useWeb3React();
+  const { value: mediaQuery } = useMediaQueryType();
   return (
-    <div className="deposit__table__body__event-box">
-      <div className="deposit__table__body__event-box__title">EVENT</div>
-      <div className="deposit__table__body__event-box__content">
-        <div>
-          <div className="bold">
-            <span>*</span> 추가 보상 이벤트
-          </div>
-          <Link to={`rewardplan/deposit`}>
-            <div className="deposit__table__body__amount__button event-button">
-              <p>자세히 보기</p>
+    <>
+      {mediaQuery === MediaQuery.PC ? (
+        <div className="deposit__table__body__event-box">
+          <div className="deposit__table__body__event-box__content">
+            <div>
+              <div className={`bold ${tokenName}`}>
+                <Trans>{t('dashboard.deposit_add_reward_event')}</Trans>
+              </div>
+              <div
+                onClick={buttonEvent}
+                className="deposit__table__body__amount__button event-button">
+                <p>{t('dashboard.claim_reward')}</p>
+              </div>
             </div>
-          </Link>
-        </div>
-        <div>
-          <div className="bold">
-            {' '}
-            {account ? (
-              <CountUp
-                className="bold amounts"
-                start={parseFloat(formatEther(expectedAddIncentiveBefore))}
-                end={parseFloat(formatEther(expectedAddIncentiveBefore))}
-                formattingFn={(number) => {
-                  return formatSixFracionDigit(number);
-                }}
-                decimals={6}
-                duration={1}
-              />
-            ) : (
-              '-'
-            )}
-            <span>{' ELFI'}</span>
+            <div>
+              <div className="bold">
+                {' '}
+                {account ? (
+                  <CountUp
+                    className="bold amounts"
+                    start={parseFloat(
+                      formatEther(expectedAdditionalIncentiveBefore),
+                    )}
+                    end={parseFloat(
+                      formatEther(expectedAdditionalIncentiveAfter),
+                    )}
+                    formattingFn={(number) => {
+                      return formatSixFracionDigit(number);
+                    }}
+                    decimals={6}
+                    duration={1}
+                  />
+                ) : (
+                  '-'
+                )}
+                <span>{' ELFI'}</span>
+              </div>
+              <p>{moneyPoolTime}</p>
+            </div>
           </div>
-          <p>{moneyPoolTime}</p>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="deposit__table__body__event-box">
+          <div className="deposit__table__body__event-box__content">
+            <div>
+              <div className={`bold ${tokenName}`}>
+                <Trans>{t('dashboard.deposit_add_reward_event')}</Trans>
+              </div>
+              <div>
+                <div className="bold">
+                  {' '}
+                  {account ? (
+                    <CountUp
+                      className="bold amounts"
+                      start={parseFloat(
+                        formatEther(expectedAdditionalIncentiveBefore),
+                      )}
+                      end={parseFloat(
+                        formatEther(expectedAdditionalIncentiveAfter),
+                      )}
+                      formattingFn={(number) => {
+                        return formatSixFracionDigit(number);
+                      }}
+                      decimals={6}
+                      duration={1}
+                    />
+                  ) : (
+                    '-'
+                  )}
+                  <span>{' ELFI'}</span>
+                </div>
+                <p>{moneyPoolTime}</p>
+              </div>
+            </div>
+            <div
+              onClick={buttonEvent}
+              className="deposit__table__body__amount__button event-button">
+              <p>{t('dashboard.claim_reward')}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default TableBodyEventReward;
