e8325af1f95b01f0eee10fe3a02d3649e08854b5
donguk.seo <donguk.seo@modori.io>
2022-01-14 21:34:56 +0900

    refactor

--- src/components/MainnetSwitch.tsx
+++ src/components/MainnetSwitch.tsx
@@ -1,7 +1,7 @@
 import { useWeb3React } from '@web3-react/core';
-import { useContext, useMemo } from "react";
+import { useContext } from "react";
 import MainnetContext from "src/contexts/MainnetContext";
-import { MainnetData, MainnetList, mainnets } from 'src/core/data/mainnets'
+import { MainnetData, MainnetList } from 'src/core/data/mainnets'
 import useCurrentChain from 'src/hooks/useCurrentChain';
 import MainnetError from 'src/assets/images/network_error.png';
 
@@ -9,66 +9,59 @@ const MainnetSwitch: React.FunctionComponent<{
   mainNetwork: boolean;
   setMainNetwork: (value: React.SetStateAction<boolean>) => void;
 }> = ({ mainNetwork, setMainNetwork }) => {
-  const { active, chainId } = useWeb3React();
-  const { 
+  const { active } = useWeb3React();
+  const {
     type: getMainnetType,
     unsupportedChainid: getMainnetError,
-    changeMainnet, 
-    setCurrentMainnet 
+    changeMainnet,
+    setCurrentMainnet
   } = useContext(MainnetContext)
   const currentChain = useCurrentChain();
 
-  const getChainData = useMemo(() => {
-    return mainnets.find((data) => {
-      return data.chainId === currentChain?.chainId
-    })
-  }, [chainId])
-  
   return (
     <div className="navigation__mainnet__container">
       <div className="navigation__mainnet__wrapper">
-        <div className={`navigation__mainnet__current ${getMainnetError ? "network-error" : ""}`} 
+        <div className={`navigation__mainnet__current ${getMainnetError ? "network-error" : ""}`}
           onClick={() => {
             setMainNetwork(!mainNetwork)
           }}
         >
-          <img 
+          <img
             src={getMainnetError ? MainnetError : MainnetData[getMainnetType].image}
           />
-          <h2>{active ? (getChainData?.name || "Wrong Network") : getMainnetType}</h2>
+          <h2>{active ? (currentChain?.name || "Wrong Network") : getMainnetType}</h2>
         </div>
         <div className={`navigation__mainnet__change-network__wrapper ${getMainnetError ? "network-error" : ""}`} style={{
           display: mainNetwork === true ? "flex" : 'none'
         }}>
           {
             MainnetList
-            .filter((data) => {
-              return (data.type !== getMainnetType) || getMainnetError
-            })
-            .map((_data, index) => {
-              return (
-                <div 
-                  key={index}
-                  className="navigation__mainnet__change-network" 
-                  onClick={() => {
-                    active ? (
-                      changeMainnet(MainnetData[_data.type].chainId).then(() => {
-                        setMainNetwork(false)
-                      })
-                    ) : (
-                      setMainNetwork(false),
-                      setCurrentMainnet(_data.type)
-                    )
-                    // window.location.reload();
-                  }}
-                >
-                  <img src={MainnetData[_data.type].image} />
-                  <h2>
-                    {MainnetData[_data.type].name}
-                  </h2>
-                </div>
-              )
-            })
+              .filter((data) => {
+                return (data.type !== getMainnetType) || getMainnetError
+              })
+              .map((_data, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="navigation__mainnet__change-network"
+                    onClick={() => {
+                      active ? (
+                        changeMainnet(MainnetData[_data.type].chainId).then(() => {
+                          setMainNetwork(false)
+                        })
+                      ) : (
+                        setMainNetwork(false),
+                        setCurrentMainnet(_data.type)
+                      )
+                    }}
+                  >
+                    <img src={MainnetData[_data.type].image} />
+                    <h2>
+                      {MainnetData[_data.type].name}
+                    </h2>
+                  </div>
+                )
+              })
           }
         </div>
       </div>
