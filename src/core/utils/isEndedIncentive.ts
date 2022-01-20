47b80d21190e819bd1f62b812f8eb7042c77dfff
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-20 14:38:27 +0900

    Modify monnet isAfter

--- src/core/utils/isEndedIncentive.ts
+++ src/core/utils/isEndedIncentive.ts
@@ -1,19 +1,15 @@
-import Token from "src/enums/Token";
+import Token from 'src/enums/Token';
 import {
-	daiMoneyPoolTime,
-	tetherMoneyPoolTime,
+  daiMoneyPoolTime,
+  tetherMoneyPoolTime,
 } from 'src/core/data/moneypoolTimes';
-import moment from "moment";
+import moment from 'moment';
 
 const isEndedIncentive = (token: string, round: number): boolean => {
-	if (token === Token.BUSD) return false;
-	const moneyPoolTime =
-		token === Token.DAI ? daiMoneyPoolTime : tetherMoneyPoolTime;
-	return moment().isAfter(
-		round === 1
-			? moneyPoolTime[round].startedAt
-			: moneyPoolTime[round].endedAt,
-	);
+  if (token === Token.BUSD) return false;
+  const moneyPoolTime =
+    token === Token.DAI ? daiMoneyPoolTime : tetherMoneyPoolTime;
+  return moment().isAfter(moneyPoolTime[round].endedAt);
 };
 
 export default isEndedIncentive;
