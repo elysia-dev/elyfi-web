9da34592e456d2d3d10fc8a31e19f170172d32f3
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-20 15:43:54 +0900

    Modify incentivePoolAddress

--- src/core/utils/getIncentivePoolAddress.ts
+++ src/core/utils/getIncentivePoolAddress.ts
@@ -1,17 +1,21 @@
-import Token from "src/enums/Token";
-import envs from "src/core/envs";
+import Token from 'src/enums/Token';
+import envs from 'src/core/envs';
 
 const getIncentivePoolAddress = (round: number, token: Token): string => {
-	switch (token) {
-		case Token.DAI:
-			return round === 1 ? envs.prevDaiIncentivePool : envs.currentDaiIncentivePool
-		case Token.USDT:
-			return round === 1 ? envs.prevDaiIncentivePool : envs.currentDaiIncentivePool
-		case Token.BUSD:
-			return envs.busdIncentivePoolAddress
-		default:
-			return envs.prevDaiIncentivePool
-	}
-}
+  switch (token) {
+    case Token.DAI:
+      return round === 1
+        ? envs.prevDaiIncentivePool
+        : envs.currentDaiIncentivePool;
+    case Token.USDT:
+      return round === 1
+        ? envs.prevUSDTIncentivePool
+        : envs.currentUSDTIncentivePool;
+    case Token.BUSD:
+      return envs.busdIncentivePoolAddress;
+    default:
+      return envs.prevDaiIncentivePool;
+  }
+};
 
-export default getIncentivePoolAddress
+export default getIncentivePoolAddress;
