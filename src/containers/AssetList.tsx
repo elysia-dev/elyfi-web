6b8369849857d8641556b76bba04a177c4881b9a
hyunoh-modori <hyunoh.bae@modori.io>
2022-01-24 18:41:09 +0900

    disable others

--- src/containers/AssetList.tsx
+++ src/containers/AssetList.tsx
@@ -1,6 +1,8 @@
 import { useHistory, useParams } from 'react-router-dom';
 import AssetItem from 'src/components/AssetItem';
 import { IAssetBond } from 'src/contexts/SubgraphContext';
+import LoanProduct from 'src/enums/LoanProduct';
+import { parseTokenId } from 'src/utiles/parseTokenId';
 
 const AssetList: React.FC<{
   assetBondTokens: IAssetBond[];
@@ -11,6 +13,9 @@ const AssetList: React.FC<{
   return (
     <div className="component__loan-list__container">
       {assetBondTokens.map((abToken, index) => {
+        const tokenId = parseTokenId(abToken.id)
+        if (LoanProduct[tokenId.productNumber] === "Others") return;
+        
         return (
           <AssetItem
             key={index}
