d726f65c00e2707bbf1c34ef7cfe94e2a299cb88
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-25 11:17:10 +0900

    Remove functions

--- src/components/RewardCountUp.tsx
+++ src/components/RewardCountUp.tsx
@@ -1,97 +1,23 @@
-import { BigNumber } from 'ethers';
 import { formatEther } from 'ethers/lib/utils';
-import moment from 'moment';
-import {
-  Dispatch,
-  FunctionComponent,
-  SetStateAction,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+import { Dispatch, FunctionComponent, SetStateAction } from 'react';
 import CountUp from 'react-countup';
-import PriceContext from 'src/contexts/PriceContext';
-import {
-  daiMoneyPoolTime,
-  tetherMoneyPoolTime,
-} from 'src/core/data/moneypoolTimes';
-import DepositBalance from 'src/core/types/DepositBalance';
-import Token from 'src/enums/Token';
-import useTvl from 'src/hooks/useTvl';
-import { GetAllReserves_reserves } from 'src/queries/__generated__/GetAllReserves';
-import calcExpectedIncentive from 'src/utiles/calcExpectedIncentive';
-import calcMiningAPR from 'src/utiles/calcMiningAPR';
+import { IReserveSubgraphData } from 'src/contexts/SubgraphContext';
+import { BalanceType } from 'src/hooks/useBalances';
+import useDepositRewardCount from 'src/hooks/useDepositRewardCount';
 import { formatSixFracionDigit } from 'src/utiles/formatters';
 
 const RewardCountUp: FunctionComponent<{
-  balance: DepositBalance;
-  reserveData: GetAllReserves_reserves;
+  balance: BalanceType;
+  reserveData: IReserveSubgraphData;
   round: number;
-  setBalances: Dispatch<SetStateAction<DepositBalance[]>>;
+  setBalances: Dispatch<SetStateAction<BalanceType[]>>;
 }> = ({ balance, reserveData, round, setBalances }) => {
-  const { value: tvl } = useTvl();
-  const { elfiPrice } = useContext(PriceContext);
-  const [incentive, setIncentive] = useState({
-    beforeIncentive: balance.incentive[round],
-    afterIncentive: balance.incentive[round],
-  });
-
-  const isEndedIncentive = (token: string, round: number) => {
-    const moneyPoolTime =
-      token === Token.DAI ? daiMoneyPoolTime : tetherMoneyPoolTime;
-
-    return round === 1
-      ? !moment().isAfter(moneyPoolTime[round].startedAt)
-      : moment().isAfter(moneyPoolTime[round].endedAt);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const calcIncentive = isEndedIncentive(balance.tokenName, round)
-        ? balance.incentive[round]
-        : balance.incentive[round].add(
-            calcExpectedIncentive(
-              elfiPrice,
-              balance.deposit,
-              calcMiningAPR(
-                elfiPrice,
-                BigNumber.from(reserveData.totalDeposit),
-              ),
-              balance.updatedAt,
-            ),
-          );
-      setIncentive({
-        ...incentive,
-        beforeIncentive: incentive.afterIncentive,
-        afterIncentive: calcIncentive,
-      });
-
-      const incentiveInfo = [incentive.afterIncentive, calcIncentive];
-      setBalances((balances) => {
-        return balances.map((item) => {
-          return balance.tokenName === Token.DAI
-            ? {
-                ...item,
-                daiIncentiveRound1:
-                  round === 0 ? incentiveInfo : item.daiIncentiveRound1,
-                daiIncentiveRound2:
-                  round === 1 ? incentiveInfo : item.daiIncentiveRound2,
-              }
-            : {
-                ...item,
-                usdtIncentiveRound1:
-                  round === 0 ? incentiveInfo : item.usdtIncentiveRound1,
-                usdtIncentiveRound2:
-                  round === 1 ? incentiveInfo : item.usdtIncentiveRound2,
-              };
-        });
-      });
-    }, 3000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [tvl, incentive, setBalances]);
+  const incentive = useDepositRewardCount(
+    balance,
+    reserveData,
+    round,
+    setBalances,
+  );
 
   return (
     <>
