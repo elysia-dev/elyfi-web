fa4dd6edceb6eca2453b49cde624aafc88faf275
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-25 14:04:53 +0900

    Modify staking reward plan

--- src/core/utils/calcMintedAmounts.ts
+++ src/core/utils/calcMintedAmounts.ts
@@ -3,10 +3,12 @@ import stakingRoundTimes from 'src/core/data/stakingRoundTimes';
 
 const calcMintedAmounts = (mintedPerDay: number): Array<number> => {
   const current = moment();
-
-  return stakingRoundTimes.map((round) => {
+  return stakingRoundTimes.map((round, index) => {
     if (current.diff(round.endedAt) > 0) {
-      return (round.endedAt.diff(round.startedAt, 'days')) * mintedPerDay;
+      return (
+        round.endedAt.diff(round.startedAt, 'days') * mintedPerDay +
+        (index === 4 ? (mintedPerDay === 1250 ? 1250 : 25000) : 0)
+      );
     }
 
     return current.diff(round.startedAt) < 0
