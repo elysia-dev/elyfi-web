348355575bf65899426d28b0238dd430b3b4f5b2
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-25 10:08:23 +0900

    Modify reward eth calculation

--- src/core/utils/calcLpReward.ts
+++ src/core/utils/calcLpReward.ts
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import Token from 'src/enums/Token';
 import { lpRoundDate } from '../data/lpStakingTime';
 
 export const ELFI_REWARD_PER_POOL = 300000;
@@ -6,15 +7,21 @@ export const ETH_REWARD_PER_POOL = 5.507;
 export const ETH_REWARD_PER_POOL_2 = 6.437;
 export const DAI_REWARD_PER_POOL = 25000;
 
-const calcReward = (max: number) => {
+const ethRewardPerPool = (index: number, max: number, token?: string) => {
+  return token === Token.ETH && index >= 1 ? ETH_REWARD_PER_POOL_2 : max;
+};
+
+const calcReward = (max: number, token?: string) => {
   const current = moment();
 
-  return lpRoundDate.map((date) => {
+  return lpRoundDate.map((date, index) => {
     if (current.diff(date.startedAt) <= 0) return 0;
 
     return current.diff(date.endedAt) <= 0
-      ? (current.diff(date.startedAt, 'seconds') * (max / 40)) / (24 * 3600)
-      : max;
+      ? (current.diff(date.startedAt, 'seconds') *
+          (ethRewardPerPool(index, max, token) / 40)) /
+          (24 * 3600)
+      : ethRewardPerPool(index, max, token);
   });
 };
 
@@ -22,11 +29,8 @@ export const calcElfiRewardByLp = (): number[] => {
   return calcReward(ELFI_REWARD_PER_POOL);
 };
 
-export const calcEthRewardByLp = (round: number): number[] => {
-  if (round === 1) {
-    return calcReward(ETH_REWARD_PER_POOL_2);
-  }
-  return calcReward(ETH_REWARD_PER_POOL);
+export const calcEthRewardByLp = (): number[] => {
+  return calcReward(ETH_REWARD_PER_POOL, Token.ETH);
 };
 
 export const calcDaiRewardByLp = (): number[] => {
