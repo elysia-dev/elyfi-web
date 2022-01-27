fd5f6dee3542032dc1132177201548041789ba42
Jung gyu cheol <kcpooh0202@naver.com>
2022-01-27 12:54:02 +0900

    Modify component

--- src/components/RewardCountUp.tsx
+++ src/components/RewardCountUp.tsx
@@ -3,7 +3,6 @@ import { Dispatch, FunctionComponent, SetStateAction } from 'react';
 import CountUp from 'react-countup';
 import { IReserveSubgraphData } from 'src/contexts/SubgraphContext';
 import { BalanceType } from 'src/hooks/useBalances';
-import useDepositRewardCount from 'src/hooks/useDepositRewardCount';
 import { formatSixFracionDigit } from 'src/utiles/formatters';
 
 const RewardCountUp: FunctionComponent<{
@@ -12,19 +11,24 @@ const RewardCountUp: FunctionComponent<{
   round: number;
   setBalances: Dispatch<SetStateAction<BalanceType[]>>;
 }> = ({ balance, reserveData, round, setBalances }) => {
-  const incentive = useDepositRewardCount(
-    balance,
-    reserveData,
-    round,
-    setBalances,
-  );
-
   return (
     <>
       <CountUp
         className="bold amounts"
-        start={parseFloat(formatEther(incentive.beforeIncentive))}
-        end={parseFloat(formatEther(incentive.afterIncentive))}
+        start={parseFloat(
+          formatEther(
+            round === 0
+              ? balance.expectedIncentiveBefore
+              : balance.expectedAdditionalIncentiveBefore,
+          ),
+        )}
+        end={parseFloat(
+          formatEther(
+            round === 0
+              ? balance.expectedIncentiveAfter
+              : balance.expectedAdditionalIncentiveAfter,
+          ),
+        )}
         formattingFn={(number) => {
           return formatSixFracionDigit(number);
         }}
