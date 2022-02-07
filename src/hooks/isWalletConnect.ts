ea907a6ae781b5f6fc9d6e752cbf9a211fd9f5be
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-07 17:12:51 +0900

    Modify chainid

--- src/hooks/isWalletConnect.ts
+++ src/hooks/isWalletConnect.ts
@@ -1,12 +1,20 @@
-import { useWeb3React } from "@web3-react/core";
-import envs from "src/core/envs";
-
+import { useWeb3React } from '@web3-react/core';
+import { useContext } from 'react';
+import envs from 'src/core/envs';
+import { Web3Context } from 'src/providers/Web3Provider';
 
 const isWalletConnect = (): boolean => {
-  const { active, chainId } = useWeb3React();
-  const isValidChain = envs.requiredNetwork === 'ganache' || (!!chainId && [envs.requiredChainId, envs.bscMainnetChainId].includes(chainId))
+  const { active, chainId } = useContext(Web3Context);
+  const isValidChain =
+    envs.requiredNetwork === 'ganache' ||
+    (!!chainId &&
+      [envs.requiredChainId, envs.bscMainnetChainId].includes(
+        chainId.toString().includes('0x')
+          ? parseInt(chainId.toString(), 16)
+          : chainId,
+      ));
 
-  return active && isValidChain
-}
+  return active && isValidChain;
+};
 
 export default isWalletConnect;
