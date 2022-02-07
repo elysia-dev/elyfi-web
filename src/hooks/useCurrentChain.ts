7308c4340bbf0c1111a166b6fe8889bc0133381e
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-07 17:12:19 +0900

    Modify chainid

--- src/hooks/useCurrentChain.ts
+++ src/hooks/useCurrentChain.ts
@@ -1,17 +1,22 @@
-import { useWeb3React } from "@web3-react/core";
-import { useMemo } from "react";
-import { mainnets } from "src/core/data/mainnets";
+import { useWeb3React } from '@web3-react/core';
+import { useContext, useMemo } from 'react';
+import { IMainnet, mainnets } from 'src/core/data/mainnets';
+import { Web3Context } from 'src/providers/Web3Provider';
 
-const useCurrentChain = () => {
-  const { chainId } = useWeb3React();
+const useCurrentChain = (): IMainnet | undefined => {
+  const { chainId } = useContext(Web3Context);
 
   const currentChain = useMemo(() => {
     return mainnets.find((mainnet) => {
-      return mainnet.chainId === chainId
-    })
-  }, [chainId])
+      return (
+        mainnet.chainId === chainId ||
+        (chainId.toString().includes('0x') &&
+          parseInt(chainId.toString(), 16) === mainnet.chainId)
+      );
+    });
+  }, [chainId]);
 
   return currentChain;
-}
+};
 
 export default useCurrentChain;
