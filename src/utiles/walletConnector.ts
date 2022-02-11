34165770fb5f36b9775d13bfc7be2d6bf2d4000a
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-11 18:44:35 +0900

    Add walletConect function

---
+++ src/utiles/walletConnector.ts
@@ -0,0 +1,32 @@
+import WalletConnectConnector from 'src/core/connectors/WalletConnector';
+import envs from 'src/core/envs';
+
+const walletConnectConnector = () => {
+  return new WalletConnectConnector({
+    rpc: {
+      1:
+        process.env.NODE_ENV === 'development'
+          ? 'https://elyfi-test.elyfi.world:8545'
+          : process.env.REACT_APP_JSON_RPC || '',
+      56: 'https://bsc-dataseed.binance.org/',
+      97: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    },
+    bridge: 'https://bridge.walletconnect.org',
+    qrcode: true,
+    pollingInterval: 1200,
+    preferredNetworkId: 1337,
+    infuraId: envs.infuraAddress,
+    qrcodeModalOptions: {
+      mobileLinks: [
+        'rainbow',
+        'metamask',
+        'argent',
+        'trust',
+        'imtoken',
+        'pillar',
+      ],
+    },
+  });
+};
+
+export default walletConnectConnector;
