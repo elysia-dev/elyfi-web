0d5b125d89953d172fb04ec6af9f5679927aa12c
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-11 19:34:31 +0900

    Modify change walletconnect mainnet

--- src/components/MainnetSwitch.tsx
+++ src/components/MainnetSwitch.tsx
@@ -9,7 +9,7 @@ const MainnetSwitch: React.FunctionComponent<{
   mainNetwork: boolean;
   setMainNetwork: (value: React.SetStateAction<boolean>) => void;
 }> = ({ mainNetwork, setMainNetwork }) => {
-  const { active } = useWeb3React();
+  const { active, chainId } = useWeb3React();
   const {
     type: getMainnetType,
     changeMainnet,
@@ -19,56 +19,59 @@ const MainnetSwitch: React.FunctionComponent<{
 
   return (
     <>
-      <ChangeNetworkGuideModal
+      {/* <ChangeNetworkGuideModal
         modalVisible={modalVisible}
         closeModal={() => {
           setModalVisible(false);
         }}
-      />
+      /> */}
       <div className="navigation__mainnet__container">
         <div className="navigation__mainnet__wrapper">
           <div
             className="navigation__mainnet__current"
             onClick={() => {
-              setModalVisible(
-                window.sessionStorage.getItem('@walletConnect') === 'true',
-              );
+              // setModalVisible(
+              //   window.sessionStorage.getItem('@walletConnect') === 'true',
+              // );
               setMainNetwork(!mainNetwork);
             }}>
             <img src={MainnetData[getMainnetType].image} />
             <h2>{getMainnetType}</h2>
           </div>
-          {window.sessionStorage.getItem('@walletConnect') === 'false' &&
-            mainNetwork === true && (
-              <div
-                className="navigation__mainnet__change-network__wrapper"
-                style={{
-                  display: mainNetwork === true ? 'flex' : 'none',
-                }}>
-                {MainnetList.filter((data) => {
-                  return data.type !== getMainnetType;
-                }).map((_data, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="navigation__mainnet__change-network"
-                      onClick={() => {
-                        active
-                          ? changeMainnet(MainnetData[_data.type].chainId).then(
-                              () => {
-                                setMainNetwork(false);
-                              },
-                            )
-                          : (setMainNetwork(false),
-                            setCurrentMainnet(_data.type));
-                      }}>
-                      <img src={MainnetData[_data.type].image} />
-                      <h2>{MainnetData[_data.type].name}</h2>
-                    </div>
-                  );
-                })}
-              </div>
-            )}
+          {mainNetwork === true && (
+            <div
+              className="navigation__mainnet__change-network__wrapper"
+              style={{
+                display: mainNetwork === true ? 'flex' : 'none',
+              }}>
+              {MainnetList.filter((data) => {
+                return data.type !== getMainnetType;
+              }).map((_data, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="navigation__mainnet__change-network"
+                    onClick={() => {
+                      active &&
+                      !(
+                        window.sessionStorage.getItem('@walletConnect') ===
+                        'true'
+                      )
+                        ? changeMainnet(MainnetData[_data.type].chainId).then(
+                            () => {
+                              setMainNetwork(false);
+                            },
+                          )
+                        : (setMainNetwork(false),
+                          setCurrentMainnet(_data.type));
+                    }}>
+                    <img src={MainnetData[_data.type].image} />
+                    <h2>{MainnetData[_data.type].name}</h2>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </>
