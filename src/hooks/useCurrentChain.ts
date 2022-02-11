98d51b03ad057a0d5a34384ea3033e20e79e9293
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-11 18:45:00 +0900

    Remove import

--- src/hooks/useCurrentChain.ts
+++ src/hooks/useCurrentChain.ts
@@ -1,7 +1,6 @@
 import { useWeb3React } from '@web3-react/core';
-import { useContext, useMemo } from 'react';
+import { useMemo } from 'react';
 import { IMainnet, mainnets } from 'src/core/data/mainnets';
-import { Web3Context } from 'src/providers/Web3Provider';
 
 const useCurrentChain = (): IMainnet | undefined => {
   const { chainId } = useWeb3React();
