7c2c91916a65c4ddfe55a9d4ab3dd0c2afcce12b
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-14 17:05:11 +0900

    Remove comment

--- src/components/MainnetSwitch.tsx
+++ src/components/MainnetSwitch.tsx
@@ -2,14 +2,12 @@ import { useWeb3React } from '@web3-react/core';
 import { useContext, useState } from 'react';
 import MainnetContext from 'src/contexts/MainnetContext';
 import { MainnetData, MainnetList } from 'src/core/data/mainnets';
-import { Web3Context } from 'src/providers/Web3Provider';
-import ChangeNetworkGuideModal from './ChangeNetworkGuideModal';
 
 const MainnetSwitch: React.FunctionComponent<{
   mainNetwork: boolean;
   setMainNetwork: (value: React.SetStateAction<boolean>) => void;
 }> = ({ mainNetwork, setMainNetwork }) => {
-  const { active, chainId } = useWeb3React();
+  const { active } = useWeb3React();
   const {
     type: getMainnetType,
     changeMainnet,
@@ -19,20 +17,11 @@ const MainnetSwitch: React.FunctionComponent<{
 
   return (
     <>
-      {/* <ChangeNetworkGuideModal
-        modalVisible={modalVisible}
-        closeModal={() => {
-          setModalVisible(false);
-        }}
-      /> */}
       <div className="navigation__mainnet__container">
         <div className="navigation__mainnet__wrapper">
           <div
             className="navigation__mainnet__current"
             onClick={() => {
-              // setModalVisible(
-              //   window.sessionStorage.getItem('@walletConnect') === 'true',
-              // );
               setMainNetwork(!mainNetwork);
             }}>
             <img src={MainnetData[getMainnetType].image} />
