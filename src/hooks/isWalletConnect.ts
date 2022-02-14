f61cdb4e220ce5886b0c5f12194189394a1ed717
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-14 23:55:31 +0900

    Modify setWalletconnect

--- src/hooks/isWalletConnect.ts
+++ src/hooks/isWalletConnect.ts
@@ -1,21 +1,11 @@
-import { useWeb3React } from '@web3-react/core';
-import { useContext } from 'react';
-import envs from 'src/core/envs';
-import { Web3Context } from 'src/providers/Web3Provider';
-
-const isWalletConnect = (): boolean => {
-  return window.sessionStorage.getItem('@walletConnect') === 'true';
-  // const { active, chainId } = useContext(Web3Context);
-  // const isValidChain =
-  //   envs.requiredNetwork === 'ganache' ||
-  //   (!!chainId &&
-  //     [envs.requiredChainId, envs.bscMainnetChainId].includes(
-  //       chainId.toString().includes('0x')
-  //         ? parseInt(chainId.toString(), 16)
-  //         : chainId,
-  //     ));
-
-  // return active && isValidChain;
+export const setWalletConnect = (wallet: string): void => {
+  window.sessionStorage.setItem('@connect', wallet);
 };
 
-export default isWalletConnect;
+export const isWalletConnector = (): boolean => {
+  return window.sessionStorage.getItem('@connect') === 'walletConnect';
+};
+
+export const isMetamask = (): boolean => {
+  return window.sessionStorage.getItem('@connect') === 'metamask';
+};
