04c8b15c9b9584bd6f7c798dd4c9293696483260
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-16 16:41:30 +0900

    Modify disconnectModal

--- src/components/WalletDisconnect.tsx
+++ src/components/WalletDisconnect.tsx
@@ -1,17 +1,20 @@
 import { useWeb3React } from '@web3-react/core';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useContext } from 'react';
+import MainnetContext from 'src/contexts/MainnetContext';
 
 type Props = {
   modalVisible: boolean;
-  //   isWrongNetwork: boolean;
+  selectWalletModalVisible: () => void;
   modalClose: () => void;
 };
 
 const WalletDisconnect: FunctionComponent<Props> = ({
   modalVisible,
+  selectWalletModalVisible,
   modalClose,
 }) => {
   const { deactivate } = useWeb3React();
+  const { type: getMainnetType } = useContext(MainnetContext);
 
   return (
     <div
@@ -20,13 +23,20 @@ const WalletDisconnect: FunctionComponent<Props> = ({
         display: modalVisible ? 'flex' : 'none',
       }}>
       <div className="wallet_select_modal__content">
+        <div>네트워크 에러</div>
+        <div>
+          엘리파이에서 {getMainnetType}를이용하기위해서는지갑에서해당
+          네트워크로변경한후에지갑을다시연결해주세요!
+        </div>
+        <div>(연결한 지갑이해당네트워크를 지원하는 지갑인지확인해주세요.)</div>
         <div
           onClick={() => {
             window.sessionStorage.setItem('@connect', 'false');
             deactivate();
             modalClose();
+            selectWalletModalVisible();
           }}>
-          연결끊기
+          지갑 연결하기
         </div>
       </div>
     </div>
