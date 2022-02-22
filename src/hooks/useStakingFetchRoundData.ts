5fad29e406581fadca8e85ea3b4f7d382dabc9f1
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-22 17:40:43 +0900

    Modify separate function

--- src/hooks/useStakingFetchRoundData.ts
+++ src/hooks/useStakingFetchRoundData.ts
@@ -1,120 +1,104 @@
 import { useWeb3React } from '@web3-react/core';
 import { BigNumber, constants } from 'ethers';
 import moment from 'moment';
-import { useContext, useEffect, useState } from 'react';
-import PriceContext from 'src/contexts/PriceContext';
-import stakingRoundTimes from 'src/core/data/stakingRoundTimes';
-import {
-  DAIPerDayOnELFIStakingPool,
-  ELFIPerDayOnELStakingPool,
-} from 'src/core/data/stakings';
+import { useContext, useEffect, useMemo, useState } from 'react';
+import MainnetContext from 'src/contexts/MainnetContext';
+import { roundTimes } from 'src/core/data/stakingRoundTimes';
 import RoundData from 'src/core/types/RoundData';
-import calcAPR from 'src/core/utils/calcAPR';
 import Token from 'src/enums/Token';
 import useStakingPool from './useStakingPool';
 
+let poolData;
+let userData;
+let rewardPool;
+let modifiedRound;
+let accountReward;
+
 const useStakingFetchRoundData = (
   stakedToken: Token.EL | Token.ELFI,
-  rewardToken: Token.ELFI | Token.DAI,
-  elPoolApr: BigNumber,
-  elfiPoolApr: BigNumber,
+  poolApr: BigNumber,
 ): {
   roundData: RoundData[];
   loading: boolean;
   error: boolean;
   fetchRoundData: (account: string | null | undefined) => Promise<void>;
 } => {
-  const { elPrice, elfiPrice } = useContext(PriceContext);
+  const { type: getMainnetType } = useContext(MainnetContext);
   const { account } = useWeb3React();
-  const { contract: stakingPool } = useStakingPool(stakedToken);
-  const { contract: stakingPoolV2, rewardContractForV2 } = useStakingPool(
-    Token.ELFI,
+  const { contract: stakingPool, rewardContractForV2 } = useStakingPool(
+    stakedToken,
     true,
   );
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [roundData, setroundData] = useState<RoundData[]>([]);
 
+  const roundTime = useMemo(
+    () => roundTimes(stakedToken, getMainnetType),
+    [getMainnetType, stakedToken],
+  );
+
   const v2Threshold = 2;
 
+  const stakedData = (
+    round: number,
+    accountReward?: BigNumber,
+    totalPrincipal?: BigNumber,
+    accountPrincipal?: BigNumber,
+  ) => {
+    return {
+      accountReward: accountReward ? accountReward : constants.Zero,
+      totalPrincipal: totalPrincipal ? totalPrincipal : constants.Zero,
+      accountPrincipal: accountPrincipal ? accountPrincipal : constants.Zero,
+      apr: poolApr,
+      loadedAt: moment(),
+      startedAt: roundTime[round].startedAt,
+      endedAt: roundTime[round].endedAt,
+    };
+  };
+
   const fetchRoundData = async (account: string | null | undefined) => {
+    setLoading(true);
     try {
       const data = await Promise.all(
-        stakingRoundTimes.map(async (_item, round) => {
-          let poolData;
-          let userData;
-          let accountReward;
-          if (stakingPoolV2 && stakingPool && account) {
+        roundTime.map(async (_item, round) => {
+          if (stakingPool && account) {
             if (
               round >= v2Threshold &&
               stakedToken === Token.ELFI &&
               rewardContractForV2
             ) {
-              const modifiedRound = (round + 1 - v2Threshold).toString();
-              poolData = await stakingPoolV2.getPoolData(modifiedRound);
-              userData = await stakingPoolV2.getUserData(
-                modifiedRound,
-                account,
-              );
-              accountReward = await rewardContractForV2.getUserReward(
-                account,
-                modifiedRound,
-              );
+              rewardPool = rewardContractForV2;
+              modifiedRound = (round + 1 - v2Threshold).toString();
             } else {
-              const modifiedRound = (round + 1).toString();
-              poolData = await stakingPool.getPoolData(modifiedRound);
-              userData = await stakingPool.getUserData(modifiedRound, account);
-              accountReward = await stakingPool.getUserReward(
-                account,
-                modifiedRound,
-              );
+              rewardPool = stakingPool;
+              modifiedRound = (round + 1).toString();
             }
-
-            return {
+            poolData = await stakingPool.getPoolData(modifiedRound);
+            userData = await stakingPool.getUserData(modifiedRound, account);
+            accountReward = await rewardPool.getUserReward(
+              account,
+              modifiedRound,
+            );
+            return stakedData(
+              round,
               accountReward,
-              totalPrincipal: poolData.totalPrincipal,
-              accountPrincipal: userData.userPrincipal,
-              apr: calcAPR(
-                poolData.totalPrincipal,
-                stakedToken === Token.EL ? elPrice : elfiPrice,
-                rewardToken === Token.ELFI
-                  ? ELFIPerDayOnELStakingPool
-                  : DAIPerDayOnELFIStakingPool,
-                rewardToken === Token.ELFI ? elfiPrice : 1,
-              ),
-              loadedAt: moment(),
-              startedAt: stakingRoundTimes[round].startedAt,
-              endedAt: stakingRoundTimes[round].endedAt,
-            } as RoundData;
+              poolData.totalPrincipal,
+              userData.userPrincipal,
+            );
           } else {
-            return {
-              accountReward: constants.Zero,
-              totalPrincipal: constants.Zero,
-              accountPrincipal: constants.Zero,
-              apr: stakedToken === Token.ELFI ? elfiPoolApr : elPoolApr,
-              loadedAt: moment(),
-              startedAt: stakingRoundTimes[round].startedAt,
-              endedAt: stakingRoundTimes[round].endedAt,
-            } as RoundData;
+            return stakedData(round);
           }
         }),
       );
-
       setroundData(data);
       setLoading(false);
     } catch (error) {
       console.log(error);
       const data = await Promise.all(
-        stakingRoundTimes.map(async (_item, round) => {
-          return {
-            accountReward: constants.Zero,
-            totalPrincipal: constants.Zero,
-            accountPrincipal: constants.Zero,
-            apr: stakedToken === Token.ELFI ? elfiPoolApr : elPoolApr,
-            loadedAt: moment(),
-            startedAt: stakingRoundTimes[round].startedAt,
-            endedAt: stakingRoundTimes[round].endedAt,
-          } as RoundData;
+        roundTime.map(async (_item, round) => {
+          return stakedData(round);
         }),
       );
       setroundData(data);
@@ -125,7 +109,7 @@ const useStakingFetchRoundData = (
 
   useEffect(() => {
     fetchRoundData(account);
-  }, [account, elfiPoolApr, elPoolApr]);
+  }, [account, poolApr, getMainnetType]);
 
   return { roundData, loading, error, fetchRoundData };
 };
