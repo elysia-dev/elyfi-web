a41993e5c2f79d37149c7bc784ac21c9498ce7c2
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-22 17:11:51 +0900

    Add poolAddress & tokenAddress

---
+++ src/utiles/stakingPoolAddress.ts
@@ -0,0 +1,37 @@
+import Token from 'src/enums/Token';
+import envs from 'src/core/envs';
+
+export const poolAddress = (
+  mainnet: string,
+  stakedToken: string,
+  v2: boolean,
+): string => {
+  switch (stakedToken) {
+    case Token.EL:
+      return envs.staking.elStakingPoolAddress;
+    case Token.ELFI:
+      if (mainnet === 'BSC') {
+        return envs.staking.elStakingPoolAddress; // elfi busd pool address
+      }
+      if (v2) {
+        return envs.staking.elfyV2StakingPoolAddress;
+      }
+      return envs.staking.elfyStakingPoolAddress;
+    default:
+      return '';
+  }
+};
+
+export const tokenAddress = (mainnet: string, stakedToken: string): string => {
+  switch (stakedToken) {
+    case Token.EL:
+      return envs.token.elAddress;
+    case Token.ELFI:
+      if (mainnet === 'BSC') {
+        return envs.token.bscElfiAddress;
+      }
+      return envs.token.governanceAddress;
+    default:
+      return envs.token.bscElfiAddress;
+  }
+};
