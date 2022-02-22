041de258174b16f39bf36f656fdb8ea0ad2892cf
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-22 17:10:48 +0900

    Add round

--- src/utiles/toOrdinalNumber.ts
+++ src/utiles/toOrdinalNumber.ts
@@ -38,6 +38,10 @@ const toOrdinalNumber = (language: string, value: number): string => {
         : language === LanguageType.ZHHANS
         ? '六'
         : '6';
+    case 7:
+      return language === LanguageType.EN ? '7th' : '7';
+    case 8:
+      return language === LanguageType.EN ? '8th' : '8';
     default:
       return '';
   }
