f4c4b6373338e30dc760ef0861016e0221ed3fb6
donguk.seo <donguk.seo@modori.io>
2022-02-24 15:20:26 +0900

    fix type error

--- src/hooks/useMoneyPool.ts
+++ src/hooks/useMoneyPool.ts
@@ -1,6 +1,6 @@
 import { useWeb3React } from '@web3-react/core';
 import { useMemo } from 'react';
-import { MoneyPool__factory } from '@elysia-dev/contract-typechain';
+import { MoneyPool__factory, MoneyPool } from '@elysia-dev/contract-typechain';
 import useCurrentMoneypoolAddress from 'src/hooks/useCurrnetMoneypoolAddress';
 
 const useMoneyPool = (): MoneyPool | undefined => {
