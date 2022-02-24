4c44688bf3e4ecc19aa27bd9931bbd75e3a363dd
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-24 19:20:15 +0900

    Remove elfi v2 contract

--- src/utiles/stakingPoolAddress.ts
+++ src/utiles/stakingPoolAddress.ts
@@ -1,11 +1,7 @@
 import Token from 'src/enums/Token';
 import envs from 'src/core/envs';
 
-export const poolAddress = (
-  mainnet: string,
-  stakedToken: string,
-  v2: boolean,
-): string => {
+export const poolAddress = (mainnet: string, stakedToken: string): string => {
   switch (stakedToken) {
     case Token.EL:
       return envs.staking.elStakingPoolAddress;
@@ -13,12 +9,9 @@ export const poolAddress = (
       if (mainnet === 'BSC') {
         return envs.staking.elfyBscStakingPoolAddress; // elfi busd pool address
       }
-      if (v2) {
-        return envs.staking.elfyV2StakingPoolAddress;
-      }
       return envs.staking.elfyStakingPoolAddress;
     default:
-      return '';
+      return envs.staking.elfyBscStakingPoolAddress;
   }
 };
 
