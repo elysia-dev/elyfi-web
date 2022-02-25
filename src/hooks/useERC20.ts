6a2955e298a436a18d588e188029e74d2e5e7305
Jung gyu cheol <kcpooh0202@naver.com>
2022-02-25 17:14:34 +0900

    Modify If Testnet

--- src/hooks/useERC20.ts
+++ src/hooks/useERC20.ts
@@ -3,15 +3,22 @@ import { useMemo } from 'react';
 import envs from 'src/core/envs';
 import { ERC20, ERC20__factory } from '@elysia-dev/contract-typechain';
 import { providers } from 'ethers';
+import useCurrentChain from './useCurrentChain';
 
 const useERC20 = (address: string): ERC20 => {
   const { library } = useWeb3React();
+  const currentChain = useCurrentChain();
+
   const contract = useMemo(() => {
     if (!library) {
       return ERC20__factory.connect(
         address,
         new providers.JsonRpcProvider(
-          address === envs.token.testBscElfiAddress
+          currentChain?.name === 'BSC Test'
+            ? address === envs.token.testBscElfiAddress
+              ? envs.jsonRpcUrl.bsc
+              : process.env.REACT_APP_JSON_RPC
+            : address === envs.token.bscElfiAddress
             ? envs.jsonRpcUrl.bsc
             : process.env.REACT_APP_JSON_RPC,
         ),
