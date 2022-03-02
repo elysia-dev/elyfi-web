7aa98357d7bef5e35d63c3dd5d172b8e7c9cec2a
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-03 01:33:37 +0900

    Modify useSWR middleware

--- src/components/MainGovernanceTable.tsx
+++ src/components/MainGovernanceTable.tsx
@@ -1,16 +1,11 @@
-import { FunctionComponent, RefObject, useEffect, useState } from 'react';
+import { FunctionComponent, RefObject, useState } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import useSWR from 'swr';
 
-import OffChainTopic, {
-  TopicList,
-  topicListFetcher,
-} from 'src/clients/OffChainTopic';
-import {
-  IOnChainToipc,
-  onChainFetcher,
-  onChainQuery,
-} from 'src/clients/OnChainTopic';
+import { topicListFetcher } from 'src/clients/OffChainTopic';
+import { onChainFetcher, onChainQuery } from 'src/clients/OnChainTopic';
+import onChainMainMiddleware from 'src/middleware/onChainMiddleware';
+import offChainMainMiddleware from 'src/middleware/offChainMiddleware';
 
 type Props = {
   governancePageY: RefObject<HTMLParagraphElement>;
@@ -21,89 +16,20 @@ const MainGovernanceTable: FunctionComponent<Props> = ({
   governancePageY,
   governancePageBottomY,
 }) => {
-  const { data: getOnChainData, error: onChainDataError } = useSWR(
-    onChainQuery,
-    onChainFetcher,
-  );
-  const { data: getOffChainData, error: offChainDataError } = useSWR(
+  const { data: onChainData } = useSWR(onChainQuery, onChainFetcher, {
+    use: [onChainMainMiddleware],
+  });
+
+  const { data: offChainData } = useSWR(
     '/proxy/c/nap/10.json',
     topicListFetcher,
+    { use: [offChainMainMiddleware] },
   );
   const [selectButton, setSelectButton] = useState(false);
-  // const [onChainLoading, setOnChainLoading] = useState(true);
-  // const [offChainLoading, setOffChainLoading] = useState(true);
-  const [onChainData, setOnChainData] = useState<
-    {
-      title: string;
-      created_at: Date;
-      link: string;
-    }[]
-  >([]);
-  const [offChainNapData, setOffChainNapData] = useState<
-    {
-      title: string;
-      created_at: Date;
-      link: string;
-    }[]
-  >([]);
+
   const [moreload, setMoreload] = useState(false);
 
   const { t } = useTranslation();
-
-  const getOnChainNAPDatas = (getOnChainData: IOnChainToipc) => {
-    const getOnChainApis = getOnChainData;
-    const getNAPCodes = getOnChainApis.proposals.filter((topic) => {
-      return topic.data.description.startsWith('NAP');
-    });
-    if (!getNAPCodes || undefined) return;
-
-    getNAPCodes.map((data) => {
-      const dates: Date = new Date(parseInt(data.timestamp, 10) * 1000);
-      const getDataId = data.id.match(/(?=).*(?=-proposal)/)?.toString();
-      return setOnChainData((_data) => [
-        ..._data,
-        {
-          title: data.data.description.match(/NAP.*/)?.toString() || '',
-          created_at: dates,
-          link: `${t('governance.link.tally')}/proposal/${getDataId}`,
-        },
-      ]);
-    });
-  };
-
-  const getOffChainNAPTitles = async (getOffChainData: TopicList) => {
-    const getOffChainApis = getOffChainData;
-    const getNAPTitles = getOffChainApis.topic_list.topics.filter((topic) => {
-      return topic.title.startsWith('NAP');
-    });
-    if (!getNAPTitles.map((title) => title.id) || undefined) return;
-    getNAPTitles
-      .map((title) => title.id)
-      .map(async (_res, _x) => {
-        const getNATData = await OffChainTopic.getTopicResult(_res);
-        const dates: Date = new Date(getNATData.data.created_at);
-        setOffChainNapData((napData) => [
-          ...napData,
-          {
-            title: getNATData.data.title,
-            created_at: dates,
-            link: `https://forum.elyfi.world/t/${getNATData.data.slug}`,
-          },
-        ]);
-      });
-  };
-
-  useEffect(() => {
-    try {
-      if (onChainDataError || offChainDataError) throw Error;
-      if (getOnChainData && getOffChainData) {
-        getOnChainNAPDatas(getOnChainData);
-        getOffChainNAPTitles(getOffChainData);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }, [getOnChainData, getOffChainData, onChainDataError, offChainDataError]);
 
   return (
     <div ref={governancePageY} className="main__governance main__section">
@@ -138,13 +64,15 @@ const MainGovernanceTable: FunctionComponent<Props> = ({
             <p>{t('main.governance.table--date')}</p>
           </div>
           {(!selectButton
-            ? [...(offChainNapData || [])].sort((a, b) => {
+            ? [...(offChainData || [])].sort((a: any, b: any) => {
                 return b.created_at! > a.created_at! ? 1 : -1;
               }) || []
-            : [...(onChainData || [])].sort((a, b) => {
+            : onChainData
+            ? [...(onChainData || [])].sort((a: any, b: any) => {
                 return b.created_at! > a.created_at! ? 1 : -1;
-              }) || []
-          ).map((_data, index) => {
+              })
+            : []
+          ).map((_data: any, index) => {
             return (
               <div
                 className="main__governance__body"
