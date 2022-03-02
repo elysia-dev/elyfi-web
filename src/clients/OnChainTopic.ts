1378a8b97f49584532f1d434bf07c4b71cc6e648
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-03 01:42:10 +0900

    Modify query & api

--- src/clients/OnChainTopic.ts
+++ src/clients/OnChainTopic.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 import { BigNumber } from 'ethers';
 import { request } from 'graphql-request';
 
@@ -18,50 +17,8 @@ export interface IOnChainToipc {
   proposals: IProposals[];
 }
 
-export const onChainFetcher = (query: string): Promise<IOnChainToipc> =>
+export const onChainFetcher = (query: string): Promise<IOnChainToipc[]> =>
   request(
     'https://api.thegraph.com/subgraphs/name/withtally/elyfi-finance-protocol',
     query,
   );
-export const onChainQuery = `
-{
-  proposals {
-    status
-    data {
-      description
-    }
-    totalVotesCast
-    totalVotesCastAgainst
-    totalVotesCastInSupport
-    totalVotesCastAbstained
-    timestamp
-    id
-  }
-}`;
-
-export class OnChainTopic {
-  static getOnChainTopicData = async (): Promise<
-    AxiosResponse<IOnChainToipc>
-  > => {
-    return axios.post(
-      'https://api.thegraph.com/subgraphs/name/withtally/elyfi-finance-protocol',
-      {
-        query: `
-      {
-        proposals {
-          status
-          data {
-            description
-          }
-          totalVotesCast
-          totalVotesCastAgainst
-          totalVotesCastInSupport
-          totalVotesCastAbstained
-          timestamp
-          id
-        }
-      }`,
-      },
-    );
-  };
-}
