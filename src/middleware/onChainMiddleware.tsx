7a8a500c720f1d4467241418e20d1cc55fb71292
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-03 16:00:57 +0900

    Add useSWR bsc middleware

--- src/middleware/onChainMiddleware.tsx
+++ src/middleware/onChainMiddleware.tsx
@@ -1,7 +1,76 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IProposals } from 'src/clients/OnChainTopic';
 import { Middleware, SWRHook } from 'swr';
+
+export const onChainGovernancBsceMiddleware: Middleware =
+  (useSWRNext: SWRHook) => (key, fetcher, config) => {
+    const swr = useSWRNext(key, fetcher, config);
+    const dataRef = useRef<any>(null);
+    const [onChainLoading, setOnChainLoading] = useState(true);
+    const [onChainData, setOnChainData] = useState<
+      {
+        title: string;
+        created_at: Date;
+        link: string;
+      }[]
+    >([]);
+
+    useEffect(() => {
+      try {
+        if (swr.data !== undefined) {
+          dataRef.current = swr.data;
+          const data: any = swr.data;
+          const getNAPDatas = data.proposals
+            .filter((proposal: any) => {
+              return proposal.state === 'active';
+            })
+            .map((data: any) => {
+              return {
+                data: {
+                  description: data.title,
+                },
+                status: data.state,
+                totalVotesCast: data.scores_total,
+                totalVotesCastAbstained: data.scores[2],
+                totalVotesCastAgainst: data.scores[1],
+                totalVotesCastInSupport: data.scores[0],
+                timestamp: data.start.toString(),
+                id: data.author,
+              } as IProposals;
+            });
+          getNAPDatas.map((data: any, index: any) => {
+            if (index !== 0) return;
+            return setOnChainData((_data: any) => {
+              return [
+                ..._data,
+                {
+                  data: {
+                    description: data.data.description
+                      .match(/\d.*(?!NAP)(?=:)/)
+                      ?.toString(),
+                  },
+                  status: data.status,
+                  totalVotesCast: data.totalVotesCast,
+                  totalVotesCastAbstained: data.totalVotesCastAbstained,
+                  totalVotesCastAgainst: data.totalVotesCastAgainst,
+                  totalVotesCastInSupport: data.totalVotesCastInSupport,
+                  id: data.id.match(/(?=).*(?=-proposal)/)?.toString(),
+                } as IProposals,
+              ];
+            });
+          });
+        }
+        setOnChainLoading(false);
+      } catch (error) {
+        setOnChainLoading(false);
+      }
+    }, [swr.data]);
+
+    const data = swr.data === undefined ? dataRef.current : onChainData;
+
+    return { ...swr, data, isValidating: onChainLoading };
+  };
 
 export const onChainGovernanceMiddleware: Middleware =
   (useSWRNext: SWRHook) => (key, fetcher, config) => {
@@ -22,7 +91,7 @@ export const onChainGovernanceMiddleware: Middleware =
           dataRef.current = swr.data;
           const getOnChainApis: any = swr.data;
           const getNAPCodes = getOnChainApis.proposals.filter((topic: any) => {
-            return topic.data.description.startsWith('NAP');
+            return topic.status === 'ACTIVE';
           });
           getNAPCodes.map((data: any) => {
             return setOnChainData((_data: any) => {
