6b4c700eb16e34b1595a2536314fe82c6ce95a95
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-11 21:24:39 +0900

    Modify useSWR

--- src/providers/SubgraphProvider.tsx
+++ src/providers/SubgraphProvider.tsx
@@ -1,64 +1,74 @@
-import { useEffect, useState } from "react";
-import { ReserveSubgraph } from "src/clients/ReserveSubgraph";
-import SubgraphContext, { IAssetBond, initialReserveSubgraph, IReserveSubgraph } from "src/contexts/SubgraphContext";
-import Loading from 'src/components/Loading';
-import MainnetType from "src/enums/MainnetType";
-import { MainnetData } from "src/core/data/mainnets";
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import {
+  bscReserveDataFetcher,
+  ethReserveDataFetcher,
+} from 'src/clients/ReserveSubgraph';
+import SubgraphContext, {
+  IAssetBond,
+  initialReserveSubgraph,
+  IReserveSubgraph,
+} from 'src/contexts/SubgraphContext';
+import MainnetType from 'src/enums/MainnetType';
+import { MainnetData } from 'src/core/data/mainnets';
+import {
+  bscReserveMiddleware,
+  ethReserveMiddleware,
+} from 'src/middleware/reservesMiddleware';
 
 const SubgraphProvider: React.FC = (props) => {
-  const [state, setState] = useState<IReserveSubgraph>(initialReserveSubgraph)
-  const [loading, setLoading] = useState(true)
+  const [state, setState] = useState<IReserveSubgraph>(initialReserveSubgraph);
+  const { data: bscReserveData, isValidating: bscLoading } = useSWR(
+    'bscReserveData',
+    bscReserveDataFetcher,
+    {
+      use: [bscReserveMiddleware],
+    },
+  );
+
+  const { data: ethReserveData, isValidating: ethLoading } = useSWR(
+    'ethReserveData',
+    ethReserveDataFetcher,
+    {
+      use: [ethReserveMiddleware],
+    },
+  );
 
   const fetchSubgraph = async () => {
-    const reserves = await Promise.all(
-      [ReserveSubgraph.getEthReserveData, ReserveSubgraph.getBscReserveData].map(async (fetcth) => {
-      const res = await fetcth();
-      return res.data.data.reserves.map((reserve) => {
-        return {
-          ...reserve,
-          assetBondTokens: res.data.data.assetBondTokens.filter((ab) => ab.reserve.id === reserve.id)
-        }
-      })
-    }))
-
+    if (!ethReserveData || !bscReserveData) return;
     setState({
       data: {
-        reserves: reserves.flat(),
-      }
-    })
-  }
+        reserves: [...bscReserveData, ...ethReserveData],
+      },
+      loading: ethLoading || bscLoading,
+    });
+  };
 
   const getAssetBondsByNetwork = (network: MainnetType): IAssetBond[] => {
     const supportedTokens = MainnetData[network].supportedTokens;
 
     return state.data.reserves.reduce((arr, reserve) => {
       if (supportedTokens.includes(reserve.id)) {
-        return [...arr, ...reserve.assetBondTokens]
+        return [...arr, ...reserve.assetBondTokens];
       } else {
-        return arr
+        return arr;
       }
-    }, [] as IAssetBond[])
-  }
+    }, [] as IAssetBond[]);
+  };
 
   useEffect(() => {
-    setLoading(true)
-    fetchSubgraph().then(() => {
-      setLoading(false)
-    })
-  }, [])
-
-  if (loading) return <Loading />;
+    fetchSubgraph();
+  }, [bscReserveData, ethReserveData]);
 
   return (
     <SubgraphContext.Provider
       value={{
         ...state,
         getAssetBondsByNetwork,
-      }}
-    >
+      }}>
       {props.children}
     </SubgraphContext.Provider>
   );
-}
+};
 
 export default SubgraphProvider;
