b07ddba341eaa19dd814dd282fe3082d27a33cf4
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-17 17:33:30 +0900

    Modify fetcher & query

--- src/clients/StakerSubgraph.ts
+++ src/clients/StakerSubgraph.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { BigNumber } from 'ethers';
 import Position from 'src/core/types/Position';
 import envs from 'src/core/envs';
+import request from 'graphql-request';
 export interface IPosition {
   data: {
     positions: Position[];
@@ -9,85 +10,72 @@ export interface IPosition {
 }
 
 export interface IPoolPosition {
-  data: {
-    daiIncentive: {
-      id: string;
-      incentivePotisions: {
-        position: {
-          id: string;
-          liquidity: BigNumber;
-        };
-      }[];
+  daiIncentive: {
+    id: string;
+    incentivePotisions: {
+      position: {
+        id: string;
+        liquidity: BigNumber;
+      };
     }[];
-    wethIncentive: {
-      id: string;
-      incentivePotisions: {
-        position: {
-          id: string;
-          liquidity: string;
-        };
-      }[];
+  }[];
+  wethIncentive: {
+    id: string;
+    incentivePotisions: {
+      position: {
+        id: string;
+        liquidity: string;
+      };
     }[];
-  };
+  }[];
 }
 
 const baseUrl = envs.subgraphApiEndpoint.stakerSubgraphURL;
 
-export class StakerSubgraph {
-  static getPositionsByOwner = async (
-    owner: string,
-  ): Promise<AxiosResponse<IPosition>> => {
-    return axios.post(baseUrl, {
-      query: `
-      {
-        positions(where: {owner: "${owner}"}){
-          id
-          tokenId
-          owner
-          staked
-          liquidity
-          incentivePotisions(where: {active: true}){
-            incentive{
-              id
-              pool
-              rewardToken
-            }
-          }
-        }
+export const positionsByOwnerFetcher = (query: string): Promise<any> =>
+  request(baseUrl, query);
+
+export const positionsByOwnerQuery = (owner: string): string => `
+{
+  positions(where: {owner: "${owner}"}){
+    id
+    tokenId
+    owner
+    staked
+    liquidity
+    incentivePotisions(where: {active: true}){
+      incentive{
+        id
+        pool
+        rewardToken
       }
-        `,
-    });
-  };
-
-  static getIncentivesWithPositionsByPoolId = async (
-    ethPoolId: string,
-    daiPoolId: string,
-  ): Promise<AxiosResponse<IPoolPosition>> => {
-    return axios.post(baseUrl, {
-      query: `
-      {
-        daiIncentive: incentives(where: {pool: "${daiPoolId}"}){
-         id
-         incentivePotisions(where: {active: true}){
-           position {
-             id,
-             liquidity,
-           }
-         }
-       }
-       wethIncentive: incentives(where: {pool: "${ethPoolId}"}){
-         id
-         incentivePotisions(where: {active: true}){
-           position {
-             id,
-             liquidity
-           }
-         }
-       }
-     }
-        `,
-    });
-  };
+    }
+  }
 }
+  `;
 
-export default StakerSubgraph;
+export const positionsByPoolIdFetcher = (query: string): Promise<any> =>
+  request(baseUrl, query);
+
+export const positionsByPoolIdQuery = `
+{
+  daiIncentive: incentives(where: {pool: "${envs.lpStaking.daiElfiPoolAddress}"}){
+   id
+   incentivePotisions(where: {active: true}){
+     position {
+       id,
+       liquidity,
+     }
+   }
+ }
+ wethIncentive: incentives(where: {pool: "${envs.lpStaking.ethElfiPoolAddress}"}){
+   id
+   incentivePotisions(where: {active: true}){
+     position {
+       id,
+       liquidity
+     }
+   }
+ }
+}
+  `;
