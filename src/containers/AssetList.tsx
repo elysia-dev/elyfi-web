f1c9370af536c3583bcca808d49be76b2d3f54ff
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-17 14:20:40 +0900

    Modify list skeleton

--- src/containers/AssetList.tsx
+++ src/containers/AssetList.tsx
@@ -1,3 +1,4 @@
+import Skeleton from 'react-loading-skeleton';
 import { useHistory, useParams } from 'react-router-dom';
 import AssetItem from 'src/components/AssetItem';
 import { IAssetBond } from 'src/core/types/reserveSubgraph';
@@ -5,7 +6,7 @@ import LoanProduct from 'src/enums/LoanProduct';
 import { parseTokenId } from 'src/utiles/parseTokenId';
 
 const AssetList: React.FC<{
-  assetBondTokens: IAssetBond[];
+  assetBondTokens?: IAssetBond[];
 }> = ({ assetBondTokens }) => {
   const history = useHistory();
   const { lng } = useParams<{ lng: string }>();
@@ -13,23 +14,27 @@ const AssetList: React.FC<{
   return (
     <div className="component__loan-list__container">
       {assetBondTokens
-        .filter((data) => {
-          const tokenId = parseTokenId(data.id);
-          return LoanProduct[tokenId.productNumber] !== 'Others';
-        })
-        .map((abToken, index) => {
-          return (
-            <AssetItem
-              key={index}
-              abToken={abToken}
-              onClick={() => {
-                history.push({
-                  pathname: `/${lng}/portfolio/${abToken.id}`,
-                });
-              }}
-            />
-          );
-        })}
+        ? assetBondTokens
+            .filter((data) => {
+              const tokenId = parseTokenId(data.id);
+              return LoanProduct[tokenId.productNumber] !== 'Others';
+            })
+            .map((abToken, index) => {
+              return (
+                <AssetItem
+                  key={index}
+                  abToken={abToken}
+                  onClick={() => {
+                    history.push({
+                      pathname: `/${lng}/portfolio/${abToken.id}`,
+                    });
+                  }}
+                />
+              );
+            })
+        : Array(3)
+            .fill(0)
+            .map(() => <Skeleton width={300} height={328.5} />)}
     </div>
   );
 };
