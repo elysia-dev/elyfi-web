3410fee2bf754177ecece0656495fed44c9fb803
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-30 16:02:40 +0900

    Add useLpStaking test

---
+++ src/hooks/useLpStaking.test.ts
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+import { useWeb3React } from '@web3-react/core';
+import { act } from 'react-test-renderer';
+import injectedConnector from 'src/core/connectors/injectedConnector';
+
+describe('useLpStaking hooks', () => {
+  test('staking', () => {});
+});
