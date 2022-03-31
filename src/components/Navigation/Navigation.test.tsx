7b54cae2bcd529c2380acc6e822c50ac82ddb516
Jung gyu cheol <kcpooh0202@naver.com>
2022-03-31 17:10:56 +0900

    Add Navigation test

---
+++ src/components/Navigation/Navigation.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { SetStateAction } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navigation from '.';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    lng: 'ko',
+  }),
+  useLocation: () => ({
+    pathname: '/ko',
+    hash: '',
+    search: '',
+    state: '',
+  }),
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string): string => str,
+    };
+  },
+}));
+
+describe('Navigation component test', () => {
+  test('navigation render', () => {
+    render(
+      <BrowserRouter>
+        <Route>
+          <Navigation
+            hamburgerBar={false}
+            setHamburgerBar={(value: SetStateAction<boolean>): void => {}}
+          />
+        </Route>
+      </BrowserRouter>,
+    );
+  });
+});
