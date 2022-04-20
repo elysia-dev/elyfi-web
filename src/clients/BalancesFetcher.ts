4ae2eecc2b3c1ff0b3b5673b1a19cc25846db7ba
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-20 19:31:55 +0900

    Add v2Poolfetcher

--- src/clients/BalancesFetcher.ts
+++ src/clients/BalancesFetcher.ts
@@ -1,18 +1,25 @@
 import { ERC20__factory } from '@elysia-dev/contract-typechain';
+import { StakingPoolV2factory } from '@elysia-dev/elyfi-v1-sdk';
 import { providers } from 'ethers';
 import envs from 'src/core/envs';
+import { getV2LPPoolContract } from 'src/utiles/v2LPPoolContract';
 
 const provider = new providers.JsonRpcProvider(process.env.REACT_APP_JSON_RPC);
 const bscProvider = new providers.JsonRpcProvider(envs.jsonRpcUrl.bsc);
+const { daiPoolContract, ethPoolContract } = getV2LPPoolContract();
+
+const erc20Contract = (address: string, provider: any) => {
+  return ERC20__factory.connect(address, provider);
+};
+const stakingPoolV2Contract = (address: string, provider: any) => {
+  return StakingPoolV2factory.connect(address, provider);
+};
 
 export const elBalanceOfFetcher =
   (): any =>
   (...args: [string]) => {
     const [...params] = args;
-    const contract: any = ERC20__factory.connect(
-      envs.token.elAddress,
-      provider as any,
-    );
+    const contract: any = erc20Contract(envs.token.elAddress, provider as any);
 
     return contract.balanceOf(params[0]);
   };
@@ -20,7 +27,7 @@ export const elfiBalanceOfFetcher =
   (): any =>
   (...args: [string]) => {
     const [...params] = args;
-    const contract: any = ERC20__factory.connect(
+    const contract: any = erc20Contract(
       envs.token.governanceAddress,
       provider as any,
     );
@@ -32,7 +39,7 @@ export const bscBalanceOfFetcher =
   (): any =>
   (...args: [string]) => {
     const [...params] = args;
-    const contract: any = ERC20__factory.connect(
+    const contract: any = erc20Contract(
       envs.token.bscElfiAddress,
       bscProvider as any,
     );
@@ -43,7 +50,7 @@ export const v2EthLPPoolElfiFetcher =
   (): any =>
   (...args: [string]) => {
     const [...params] = args;
-    const contract: any = ERC20__factory.connect(
+    const contract: any = erc20Contract(
       envs.token.governanceAddress,
       provider as any,
     );
@@ -54,7 +61,7 @@ export const v2LDaiLPPoolElfiFetcher =
   (): any =>
   (...args: [string]) => {
     const [...params] = args;
-    const contract: any = ERC20__factory.connect(
+    const contract: any = erc20Contract(
       envs.token.governanceAddress,
       provider as any,
     );
@@ -65,10 +72,8 @@ export const v2LPPoolDaiFetcher =
   (): any =>
   (...args: [string]) => {
     const [...params] = args;
-    const contract: any = ERC20__factory.connect(
-      envs.token.wEthAddress,
-      provider as any,
-    );
+
+    const contract: any = erc20Contract(envs.token.daiAddress, provider as any);
 
     return contract.balanceOf(params[0]);
   };
@@ -76,10 +81,52 @@ export const v2LPPoolEthFetcher =
   (): any =>
   (...args: [string]) => {
     const [...params] = args;
-    const contract: any = ERC20__factory.connect(
-      envs.token.daiAddress,
+    const contract: any = erc20Contract(
+      envs.token.wEthAddress,
       provider as any,
     );
 
     return contract.balanceOf(params[0]);
+  };
+export const elfiV2BalanceFetcher =
+  (): any =>
+  (...args: [string]) => {
+    const [...params] = args;
+    const contract: any = stakingPoolV2Contract(
+      envs.stakingV2MoneyPool.elfiStaking,
+      provider as any,
+    );
+
+    return contract.getPoolData();
+  };
+export const elfiBscV2BalanceFetcher =
+  (): any =>
+  (...args: [string]) => {
+    const [...params] = args;
+    const contract: any = stakingPoolV2Contract(
+      envs.stakingV2MoneyPool.elfiBscStaking,
+      bscProvider as any,
+    );
+
+    return contract.getPoolData();
+  };
+export const ethPoolDataFetcher =
+  (): any =>
+  (...args: [string]) => {
+    return ethPoolContract.getPoolData();
+  };
+export const daiPoolDataFetcher =
+  (): any =>
+  (...args: [string]) => {
+    return daiPoolContract.getPoolData();
+  };
+export const ethTotalSupplyFetcher =
+  (): any =>
+  (...args: [string]) => {
+    return ethPoolContract.totalSupply();
+  };
+export const daiTotalSupplyFetcher =
+  (): any =>
+  (...args: [string]) => {
+    return daiPoolContract.totalSupply();
   };
