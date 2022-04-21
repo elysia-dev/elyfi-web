d0c0198496cb20315bc04dbb97d5dbf6c1e7e5f3
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-21 15:50:25 +0900

    Modify tvl data

--- src/hooks/useTvl.ts
+++ src/hooks/useTvl.ts
@@ -6,6 +6,7 @@ import envs from 'src/core/envs';
 import { pricesFetcher } from 'src/clients/Coingecko';
 import ReserveData from 'src/core/data/reserves';
 import priceMiddleware from 'src/middleware/priceMiddleware';
+import { v2LPPoolElfiFetcher } from 'src/clients/BalancesFetcher';
 import useReserveData from './useReserveData';
 import useTvlBalances from './useTvlBalances';
 
@@ -22,17 +23,23 @@ const useTvl = (): { value: number; loading: boolean } => {
     },
   );
 
+  const { data: v2LPPoolElfi } = useSWR(
+    [envs.lpStaking.ethElfiV2PoolAddress, envs.lpStaking.daiElfiV2PoolAddress],
+    {
+      fetcher: v2LPPoolElfiFetcher(),
+    },
+  );
+
   const tvl = useMemo(() => {
     try {
-      if (!priceData || reserveLoading || state.balanceLoading) return 0;
+      if (!priceData || reserveLoading || state.balanceLoading || !v2LPPoolElfi)
+        return 0;
 
       const stakedTokenElfiEthPrice =
-        parseFloat(utils.formatEther(state.v2EthLPPoolElfi)) *
-          priceData.elfiPrice +
+        parseFloat(utils.formatEther(v2LPPoolElfi[0])) * priceData.elfiPrice +
         parseFloat(utils.formatEther(state.v2LPPoolEth)) * priceData.ethPrice;
       const stakedTokenElfiDaiPrice =
-        parseFloat(utils.formatEther(state.v2DaiLPPoolElfi)) *
-          priceData.elfiPrice +
+        parseFloat(utils.formatEther(v2LPPoolElfi[1])) * priceData.elfiPrice +
         parseFloat(utils.formatEther(state.v2LPPoolDai)) * priceData.daiPrice;
 
       const ethPoolPerToken =
@@ -69,18 +76,7 @@ const useTvl = (): { value: number; loading: boolean } => {
         state.ethPool.totalValueLockedToken1 * priceData.ethPrice +
         state.daiPool.totalValueLockedToken0 * priceData.elfiPrice +
         state.daiPool.totalValueLockedToken1 * priceData.daiPrice +
-        parseInt(
-          formatEther(
-            state.v1StakingBalance
-              .add(state.v2StakingBalance)
-              .add(state.elfiV2Balance)
-              .add(state.elfiBscV2Balance)
-              .add(state.bscStakingBalance)
-              .add(state.v2DaiLPPoolElfi.add(state.v2EthLPPoolElfi)),
-          ),
-          10,
-        ) *
-          priceData.elfiPrice +
+        parseInt(formatEther(state.stakingBalance), 10) * priceData.elfiPrice +
         parseInt(formatEther(state.v2LPPoolEth), 10) * priceData.ethPrice +
         parseInt(formatEther(state.v2LPPoolDai), 10) * priceData.daiPrice +
         ethTotalUSD +
