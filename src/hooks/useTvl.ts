3f0f181941e80a7ce5750779cc3a1aff3c6bbd10
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-26 17:41:09 +0900

    Add tvl api

--- src/hooks/useTvl.ts
+++ src/hooks/useTvl.ts
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react';
+import { tvlFetcher } from 'src/clients/BalancesFetcher';
 import useSWR from 'swr';
-import { tvlBalanceFetcher } from 'src/clients/BalancesFetcher';
 
 const useTvl = (): { value: number; loading: boolean } => {
   const [loading, setLoading] = useState(true);
   const [tvl, setTvl] = useState(0);
 
-  const { data, error } = useSWR(
-    process.env.REACT_APP_TVL_URL,
-    tvlBalanceFetcher,
-  );
+  const { data, error } = useSWR(process.env.REACT_APP_TVL_URL, tvlFetcher);
 
   useEffect(() => {
     if (!data && !error) return;
