d0b27f6a2d4b39b53d28fd08287df2c9f174b31f
hyunoh-modori <hyunoh.bae@modori.io>
2022-05-02 11:33:21 +0900

    change provider signer

--- src/components/Staking/hooks/useStakingPoolV2.ts
+++ src/components/Staking/hooks/useStakingPoolV2.ts
@@ -1,10 +1,12 @@
 import { useWeb3React } from '@web3-react/core';
 import { useContext, useMemo } from 'react';
 import { StakingPoolV2, StakingPoolV2factory } from '@elysia-dev/elyfi-v1-sdk';
+import { providers } from 'ethers';
 import envs from 'src/core/envs';
 import Token from 'src/enums/Token';
 import { poolAddressV2 } from 'src/utiles/stakingPoolAddress';
 import MainnetContext from 'src/contexts/MainnetContext';
+import MainnetType from 'src/enums/MainnetType';
 
 const useStakingPoolV2 = (
   staked: Token,
@@ -13,11 +15,16 @@ const useStakingPoolV2 = (
 } => {
   const { library } = useWeb3React();
   const { type: getMainnetType } = useContext(MainnetContext);
+
   const contract = useMemo(() => {
     if (!library) return;
     return StakingPoolV2factory.connect(
       poolAddressV2(getMainnetType, staked),
-      library.getSigner(),
+      new providers.JsonRpcProvider(
+        getMainnetType === MainnetType.BSC
+          ? envs.jsonRpcUrl.bsc
+          : process.env.REACT_APP_JSON_RPC,
+      ) as any,
     );
   }, [library, staked, getMainnetType]);
 
