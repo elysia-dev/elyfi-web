c97da4c4e47cb8c508f1f912c6a025ffc8cc46d9
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-31 19:09:01 +0900

    Add usdc

--- src/core/types/ReserveToken.ts
+++ src/core/types/ReserveToken.ts
@@ -1,5 +1,5 @@
 import Token from 'src/enums/Token';
 
-type ReserveToken = Token.DAI | Token.BUSD | Token.USDT;
+type ReserveToken = Token.DAI | Token.BUSD | Token.USDT | Token.USDC;
 
 export default ReserveToken;
