5a7d07481248281489faeb3514c14a20eea8c39c
Jung Gyu Cheol <kcpooh0202@naver.com>
2022-06-02 11:11:32 +0900

    Add usdc

--- src/core/utils/isSupportedReserve.ts
+++ src/core/utils/isSupportedReserve.ts
@@ -4,7 +4,7 @@ import Token from 'src/enums/Token';
 const isSupportedReserve = (token: Token, network: MainnetType): boolean => {
   switch (network) {
     case MainnetType.Ethereum:
-      return [Token.DAI, Token.USDT].includes(token);
+      return [Token.DAI, Token.USDT, Token.USDC].includes(token);
     case MainnetType.BSC:
       return [Token.BUSD].includes(token);
     default:
