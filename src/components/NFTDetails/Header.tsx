7d3a8726335ae7c8361f58aef2f96bb87e87c07f
hyunoh-modori <hyunoh.bae@modori.io>
2022-07-13 20:57:33 +0900

    add mobile

--- src/components/NFTDetails/Header.tsx
+++ src/components/NFTDetails/Header.tsx
@@ -22,7 +22,7 @@ const Header: React.FC<Props> = ({ onButtonClick }) => {
         <h1>{t('nftMarket.title')}</h1>
         <p>{t('nftMarket.subTitle')}</p>
       </div>
-      <section>
+      <section className="pc-only">
         <div>
           <b>
             {t('nftMarket.myPurchase')}
@@ -47,6 +47,32 @@ const Header: React.FC<Props> = ({ onButtonClick }) => {
           <button onClick={onButtonClick}>{t('nftMarket.purchase')}</button>
         </div>
         <b>{currentPurchaseAmount()}</b>
+      </section>
+      <section className="mobile-only">
+        <div>
+          <b>
+            {t('nftMarket.myPurchase')}
+            <span>
+              <Questionmark
+                content={
+                  <Trans i18nKey={'nftMarket.myPurchaseInfo'}>
+                    text
+                    <u>
+                      <a
+                        target="_blank"
+                        href="https://etherscan.io/"
+                        style={{ color: '#00bfff' }}>
+                        link
+                      </a>
+                    </u>
+                  </Trans>
+                }
+              />
+            </span>
+          </b>
+          <span>{currentPurchaseAmount()}</span>
+        </div>
+        <button onClick={onButtonClick}>{t('nftMarket.purchase')}</button>
       </section>
     </>
   );
