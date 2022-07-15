fda4c5d4844458358e1d708144dca794dd66a1a8
Jung gyu cheol <kcpooh0202@naver.com>
2022-07-15 17:13:37 +0900

    Modify handler

--- src/components/Market/Modals/components/PurchaseButton.tsx
+++ src/components/Market/Modals/components/PurchaseButton.tsx
@@ -2,19 +2,25 @@ interface WalletBtnType {
   content: string;
   isLoading?: boolean;
   onClickHandler: () => void;
+  isPayAmount: boolean | undefined;
 }
 
 const PurchaseButton: React.FC<WalletBtnType> = ({
   content,
   isLoading,
   onClickHandler,
+  isPayAmount,
 }) => {
   return (
     <div
       className={`market_modal__purchase_btn ${
         isLoading && 'purchase_loading'
       }`}>
-      <button onClick={() => onClickHandler()}>{content}</button>
+      <button
+        style={{ background: isPayAmount ? '#e6e6e6' : undefined }}
+        onClick={() => onClickHandler()}>
+        {content}
+      </button>
     </div>
   );
 };
