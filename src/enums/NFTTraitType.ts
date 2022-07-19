5f5c246a3a1ffa59b0489eaef05dbfcb3b2ce03e
Jung gyu cheol <kcpooh0202@naver.com>
2022-07-19 14:09:17 +0900

    Add NFTTraitType

---
+++ src/enums/NFTTraitType.ts
@@ -0,0 +1,9 @@
+enum NFTTraitType {
+  Principal = 'Principal',
+  ExpectedAPY = 'Expected APY',
+  OverdueAPY = 'Overdue APY',
+  LoanDate = 'Loan Date',
+  MaturityDate = 'Maturity Date',
+}
+
+export default NFTTraitType;
