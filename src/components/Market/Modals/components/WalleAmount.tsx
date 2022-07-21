0b3ac7d2847b6c7979ad82be11adfab07b3c9caf
Jung gyu cheol <kcpooh0202@naver.com>
2022-07-21 17:57:14 +0900

    Modify span

--- src/components/Market/Modals/components/WalleAmount.tsx
+++ src/components/Market/Modals/components/WalleAmount.tsx
@@ -20,7 +20,9 @@ const WalletAmount: React.FC<Amount> = ({ balances, remainingNFT }) => {
     <div className="market_modal__amount">
       <div>
         <div>{t('nftModal.purchaseModal.remainigNFT')}</div>
-        <div>{formatCommaSmallZeroDisits(remainingNFT)} NFT(s)</div>
+        <div>
+          {formatCommaSmallZeroDisits(remainingNFT)} <span>NFT(s)</span>
+        </div>
       </div>
       <div>
         <div>{t('nftModal.purchaseModal.wallet')}</div>
