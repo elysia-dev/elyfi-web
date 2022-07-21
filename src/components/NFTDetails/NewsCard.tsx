d07008823569160d6b5b757edb6e9336cd910fe3
hyunoh-modori <hyunoh.bae@modori.io>
2022-07-21 18:35:33 +0900

    add page interaction

--- src/components/NFTDetails/NewsCard.tsx
+++ src/components/NFTDetails/NewsCard.tsx
@@ -10,14 +10,12 @@ const NewsCard: React.FC<Props> = ({ index, data }) => {
   const { t } = useTranslation();
 
   return (
-    <section key={index}>
+    <a key={index} target="_blank" href={data.link}>
       <img src={data.image} alt="News image" />
       <b>{data.title}</b>
       <p>{data.content}</p>
-      <a target="_blank" href={data.link}>
-        {t('nftMarket.newsButton')}
-      </a>
-    </section>
+      <a>{t('nftMarket.newsButton')}</a>
+    </a>
   );
 };
 
